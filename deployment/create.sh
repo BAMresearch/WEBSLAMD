70b7015e3fe2867a6ed8766b624cc16d3469ed90
RugTehseen <tehseen.rug@iteratec.com>
2022-06-23 09:02:40 +0200

    slamd-0 - script for creating an app

---
+++ deployment/create.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+APP_NAME=${1}
+STAGE_NAME=${2}
+
+if [[ $# -ne 2 ]]; then
+  echo "You need to provide name and stage of your app "
+  exit
+fi
+
+SCRIPT_DIR="$(cd -- "$(dirname -- "$0")" && pwd)"
+
+cd ${SCRIPT_DIR}/..
+
+heroku create ${APP_NAME} --remote ${STAGE_NAME}
