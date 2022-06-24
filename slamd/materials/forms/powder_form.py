dff02e09ca4d3d0ac70af0dcafb606349f05780d
Michael Hoepler <michael.hoepler@iteratec.com>
2022-06-24 09:38:57 +0200

    Adding ingredients to the powder form

--- slamd/materials/forms/powder_form.py
+++ slamd/materials/forms/powder_form.py
@@ -3,5 +3,39 @@ from wtforms import DecimalField
 
 
 class PowderForm(Form):
-
-    ingredient = DecimalField(label='FeO')
+    feo = DecimalField(
+        label='Fe2O3 (%m)'
+    )
+    sio = DecimalField(
+        label='SiO2 (%m)'
+    )
+    alo = DecimalField(
+        label='Al2O3 (%m)'
+    )
+    cao = DecimalField(
+        label='Al2O3 (%m)'
+    )
+    mgo = DecimalField(
+        label='Al2O3 (%m)'
+    )
+    nao = DecimalField(
+        label='Al2O3 (%m)'
+    )
+    ko = DecimalField(
+        label='K2O (%m)'
+    )
+    so = DecimalField(
+        label='SO3 (%m)'
+    )
+    tio = DecimalField(
+        label='TiO2 (%m)'
+    )
+    po = DecimalField(
+        label='P2O5 (%m)'
+    )
+    sro = DecimalField(
+        label='SrO (%m)'
+    )
+    mno = DecimalField(
+        label='Mn2O3 (%m)'
+    )
