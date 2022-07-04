5b3e77ba3bd527933ab142b160179b3ce6a7a75d
RugTehseen <tehseen.rug@iteratec.com>
2022-07-04 13:37:25 +0200

    slamd-7: remove dead code

--- slamd/materials/materials_persistence.py
+++ slamd/materials/materials_persistence.py
@@ -1,17 +1,7 @@
 from flask import session
 
-from slamd.materials.material_type import MaterialType
-
 
 class MaterialsPersistence:
-
-    @classmethod
-    def find_all(cls):
-        all_materials = []
-        all_material_types = MaterialType.get_all_types()
-        for material_type in all_material_types:
-            all_materials.extend(session.get(f'{material_type.lower()}_list', []))
-        return all_materials
 
     @classmethod
     def save(cls, material_type, material):
