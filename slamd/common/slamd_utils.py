01c731f3ac0823ed172052e5407bdb1975c60b46
RugTehseen <tehseen.rug@iteratec.com>
2022-07-04 11:55:55 +0200

    slamd-7: fix typo

--- slamd/common/slamd_utils.py
+++ slamd/common/slamd_utils.py
@@ -14,7 +14,7 @@ def join_all(input_list):
 
 def molecular_formula_of(input_molecule):
     """
-    input_molecule: pass molecule as simple string such as H20 to get in back in proper chemical notation
+    input_molecule: pass molecule as simple string such as H20 to get it back in proper chemical notation
     """
     subscript = str.maketrans("0123456789", "₀₁₂₃₄₅₆₇₈₉")
     return input_molecule.translate(subscript)
