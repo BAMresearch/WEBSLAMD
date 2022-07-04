41f1dca11620c10cddcaa6132d67f7e43b5b2c8d
Horacio Scaffino <horacio.scaffino@iteratec.com>
2022-07-04 17:02:03 +0200

    Rename chemical formula variables for liquids

--- slamd/materials/forms/liquid_form.py
+++ slamd/materials/forms/liquid_form.py
@@ -4,39 +4,50 @@ from slamd.materials.forms.base_materials_form import BaseMaterialsForm
 
 
 class LiquidForm(BaseMaterialsForm):
-    nasio = DecimalField(
+    na2_si_o3 = DecimalField(
         label='Na2SiO3 (%m)'
     )
-    naoh = DecimalField(
+
+    na_o_h = DecimalField(
         label='NaOH (%m)'
     )
-    nasio_specific = DecimalField(
+
+    na2_si_o3_specific = DecimalField(
         label='Na2SiO3 specific (%m)'
     )
-    naoh_specific = DecimalField(
+
+    na_o_h_specific = DecimalField(
         label='NaOH specific (%m)'
     )
+
     total = DecimalField(
         label='Total solution (%m)'
     )
-    nao = DecimalField(
+
+    na2_o = DecimalField(
         label='Na2O (I) (%)'
     )
-    sio = DecimalField(
+
+    si_o2 = DecimalField(
         label='SiO2 (I) (%)'
     )
-    ho = DecimalField(
+
+    h2_o = DecimalField(
         label='H2O (%)'
     )
-    nao_dry = DecimalField(
+
+    na2_o_dry = DecimalField(
         label='Na2O (dry) (%m)'
     )
-    sio_dry = DecimalField(
+
+    si_o2_dry = DecimalField(
         label='SiO2 (dry) (%m)'
     )
+
     water = DecimalField(
         label='Water (%m)'
     )
-    naoh_total = DecimalField(
+
+    na_o_h_total = DecimalField(
         label='Total NaOH (%m)'
     )
