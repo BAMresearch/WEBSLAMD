88a9b08518c310dc4d6f5c682ddd2cfaa482f546
Ghezal Ahmad Zia <ghezalahmad.zia@outlook.com>
2022-07-04 15:24:07 +0200

    Custom Form properties added

--- slamd/materials/forms/custom_form.py
+++ slamd/materials/forms/custom_form.py
@@ -5,97 +5,15 @@ from slamd.materials.forms.base_materials_form import BaseMaterialsForm
 
 class CustomForm(BaseMaterialsForm):
 
-    feo = DecimalField(
-        label='Fe2O3 (%m)',
+    name = DecimalField(
+        label='Name:',
         validators=[
             validators.Optional()
         ]
     )
 
-    sio = DecimalField(
-        label='SiO2 (%m)',
-        validators=[
-            validators.Optional()
-        ]
-    )
-
-    alo = DecimalField(
-        label='Al2O3 (%m)',
-        validators=[
-            validators.Optional()
-        ]
-    )
-
-    cao = DecimalField(
-        label='CaO (%m)',
-        validators=[
-            validators.Optional()
-        ]
-    )
-    mgo = DecimalField(
-        label='MgO (%m)',
-        validators=[
-            validators.Optional()
-        ]
-    )
-
-    nao = DecimalField(
-        label='Na2O (%m)',
-        validators=[
-            validators.Optional()
-        ]
-    )
-    ko = DecimalField(
-        label='K2O (%m)',
-        validators=[
-            validators.Optional()
-        ]
-    )
-
-    so = DecimalField(
-        label='SO3 (%m)',
-        validators=[
-            validators.Optional()
-        ]
-    )
-
-    tio = DecimalField(
-        label='TiO2 (%m)',
-        validators=[
-            validators.Optional()
-        ]
-    )
-
-    po = DecimalField(
-        label='P2O5 (%m)',
-        validators=[
-            validators.Optional()
-        ]
-    )
-
-    sro = DecimalField(
-        label='SrO (%m)',
-        validators=[
-            validators.Optional()
-        ]
-    )
-
-    mno = DecimalField(
-        label='Mn2O3 (%m)',
-        validators=[
-            validators.Optional()
-        ]
-    )
-
-    fine = DecimalField(
-        label='Fine modules (m2/kg)',
-        validators=[
-            validators.Optional()
-        ]
-    )
-
-    gravity = DecimalField(
-        label='Specific gravity (%m)',
+    value = DecimalField(
+        label='Value:',
         validators=[
             validators.Optional()
         ]
