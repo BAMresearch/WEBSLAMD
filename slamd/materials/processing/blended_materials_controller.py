9476a3c92629a6acdcc562889a68166e2e58d058
RugTehseen <tehseen.rug@iteratec.com>
2022-07-07 12:50:59 +0200

    slamd-12: add token and increment field

--- slamd/materials/processing/blended_materials_controller.py
+++ slamd/materials/processing/blended_materials_controller.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect
 
 from slamd.materials.processing.forms.blending_form import BlendingForm
 
@@ -14,3 +14,13 @@ blended_materials_service = None
 @blended_materials.route('', methods=['GET'])
 def blended_material_page():
     return render_template('blended_materials.html', form=BlendingForm())
+
+
+@blended_materials.route('', methods=['POST'])
+def submit_blending():
+    blending_data = BlendingForm(request.form)
+
+    if blending_data.validate():
+        return redirect('/materials/blended')
+
+    return render_template('blended_materials.html', form=blending_data)
--- slamd/materials/processing/forms/blending_form.py
+++ slamd/materials/processing/forms/blending_form.py
@@ -1,13 +1,17 @@
-from wtforms import Form, StringField, validators, SubmitField
+from flask_wtf import FlaskForm as Form
+from wtforms import StringField, validators, SubmitField, DecimalField
 
 
 class BlendingForm(Form):
-
     # TODO: custom validator
     blended_material_name = StringField(
         label='Name',
         validators=[validators.DataRequired(
             message='Material name cannot be empty')]
+    )
+
+    increment = DecimalField(
+        label='Increment (%)'
     )
 
     submit = SubmitField('Create blended materials')
