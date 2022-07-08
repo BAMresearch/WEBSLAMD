dd9e000bed10bcdd7607aa14081a29b66f6b3ee2
RugTehseen <tehseen.rug@iteratec.com>
2022-07-08 16:09:23 +0200

    slamd-12: test for changing type of base material for later blending

--- tests/materials/processing/test_blended_materials_controller.py
+++ tests/materials/processing/test_blended_materials_controller.py
@@ -1,9 +1,11 @@
+import json
+
 from slamd import create_app
 from slamd.materials.processing.materials_persistence import MaterialsPersistence
-from tests.materials.materials_test_data import create_test_powders
+from tests.materials.materials_test_data import create_test_powders, create_test_aggregates
 
 
-def test_blended_materials_controller_show_initial_blended_materials_form(client, monkeypatch):
+def test_blended_materials_controller_shows_initial_blended_materials_form(client, monkeypatch):
     app = create_app('testing', with_session=False)
 
     def mock_query_by_type(input):
@@ -25,3 +27,26 @@ def test_blended_materials_controller_show_initial_blended_materials_form(client
     assert 'Material type' in html
     assert 'test powder' in html
     assert 'my powder' in html
+
+
+def test_blended_materials_controller_loads_correct_selection_after_choosing_type(client, monkeypatch):
+    app = create_app('testing', with_session=False)
+
+    def mock_query_by_type(input):
+        if input == 'aggregates':
+            return create_test_aggregates()
+        return []
+
+    monkeypatch.setattr(MaterialsPersistence, 'query_by_type', mock_query_by_type)
+
+    with app.test_request_context('/materials/blended'):
+
+        response = client.get('/materials/blended/aggregates')
+
+    assert response.status_code == 200
+
+    template = json.loads(response.data.decode('utf-8'))['template']
+    assert 'test aggregate' in template
+    assert 'Blended base materials' not in template
+    assert 'Name' not in template
+    assert 'Material type' not in template
