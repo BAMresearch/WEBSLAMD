969a8a44be199dd986080414b05ede03019a9768
RugTehseen <tehseen.rug@iteratec.com>
2022-07-08 15:13:01 +0200

    slamd-12: add exception handling when requesting unsupported type

--- slamd/materials/processing/blended_materials_service.py
+++ slamd/materials/processing/blended_materials_service.py
@@ -1,13 +1,16 @@
-from wtforms import FormField
-
+from slamd.common.error_handling import MaterialNotFoundException
 from slamd.materials.processing.forms.base_material_selection_form import BaseMaterialSelectionForm
-from slamd.materials.processing.forms.min_max_form import MinMaxForm, MinMaxEntriesForm
+from slamd.materials.processing.forms.min_max_form import MinMaxForm
+from slamd.materials.processing.material_type import MaterialType
 from slamd.materials.processing.materials_persistence import MaterialsPersistence
 
 
 class BlendedMaterialsService:
 
     def list_material_selection_by_type(self, material_type):
+        if material_type not in MaterialType.get_all_types():
+            raise MaterialNotFoundException('The requested type is not supported!')
+
         materials_by_type = MaterialsPersistence.query_by_type(material_type)
 
         material_selection = []
