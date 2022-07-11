d596a134ca87cacec7a49e5fc92134c3b1475a09
Horacio Scaffino <horacio.scaffino@iteratec.com>
2022-07-11 14:21:00 +0200

    Return correct HTTP status code on error

--- slamd/common/error_handling.py
+++ slamd/common/error_handling.py
@@ -2,8 +2,8 @@ from flask import render_template
 
 
 def handle_404(err):
-    return render_template('404.html')
+    return render_template('404.html'), 404
 
 
 def handle_400(err):
-    return render_template('400.html')
+    return render_template('400.html'), 400
