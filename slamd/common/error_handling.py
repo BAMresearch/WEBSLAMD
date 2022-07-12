2736aebc8ad7da2aa2484811e7275a3eee502b8d
RugTehseen <tehseen.rug@iteratec.com>
2022-07-12 13:26:23 +0200

    slamd-12: fix error handling

--- slamd/common/error_handling.py
+++ slamd/common/error_handling.py
@@ -3,11 +3,17 @@ from werkzeug.exceptions import NotFound, BadRequest
 
 
 def handle_404(err):
-    return render_template('404.html', message=err.message), 404
+    try:
+        return render_template('404.html', message=err.message), 404
+    except AttributeError:
+        return render_template('404.html', message='The requested page is not available.'), 404
 
 
 def handle_400(err):
-    return render_template('400.html', message=err.message), 400
+    try:
+        return render_template('400.html', message=err.message), 400
+    except AttributeError:
+        return render_template('400.html', message='You are not allowed to access the requested resource.'), 400
 
 
 class MaterialNotFoundException(NotFound):
