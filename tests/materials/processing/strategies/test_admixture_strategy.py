061d24155a17de0d9910b2f3dd0ab5161f56bb85
Horacio Scaffino <horacio.scaffino@iteratec.com>
2022-07-12 11:11:44 +0200

    Add tests for gather_composition_properties for all strategies

---
+++ tests/materials/processing/strategies/test_admixture_strategy.py
@@ -0,0 +1,18 @@
+from slamd.materials.processing.models.admixture import Admixture
+from slamd.materials.processing.models.material import Costs
+from slamd.materials.processing.strategies.admixture_strategy import AdmixtureStrategy
+
+
+def test_gather_composition_properties_adds_all_properties():
+    strategy = AdmixtureStrategy()
+    admixture = Admixture(
+        name='test admixture',
+        type='Admixture',
+        costs=Costs(),
+        additional_properties=[],
+        composition=10.4,
+        admixture_type='test type'
+    )
+    
+    result = strategy.gather_composition_information(admixture)
+    assert result == ['Composition: 10.4, ', 'Type: test type, ']
