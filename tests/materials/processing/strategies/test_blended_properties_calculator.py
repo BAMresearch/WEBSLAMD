4f589abc3d4b20039ae5521c6f0dc490e2c485a4
RugTehseen <tehseen.rug@iteratec.com>
2022-07-20 10:27:54 +0200

    slamd-25: tests for computing mean

--- tests/materials/processing/strategies/test_blended_properties_calculator.py
+++ tests/materials/processing/strategies/test_blended_properties_calculator.py
@@ -1,4 +1,5 @@
 from slamd.materials.processing.models.material import Costs
+from slamd.materials.processing.models.powder import Composition
 from slamd.materials.processing.strategies.blending_properties_calculator import BlendingPropertiesCalculator
 
 
@@ -30,3 +31,37 @@ def test_compute_blended_costs_correctly_computes_all_cost_properties_when_not_a
     assert costs.co2_footprint == '47.17'
     assert costs.costs is None
     assert costs.delivery_time == '80.0'
+
+
+def test_compute_mean_finds_weighted_average_if_all_values_are_filled():
+    first_material_as_dict = {
+        'composition': Composition(fe3_o2='1.2')
+    }
+    second_material_as_dict = {
+        'composition': Composition(fe3_o2='3.6')
+    }
+    third_material_as_dict = {
+        'composition': Composition(fe3_o2='30')
+    }
+    base_materials_as_dict = [first_material_as_dict, second_material_as_dict, third_material_as_dict]
+    mean = BlendingPropertiesCalculator.compute_mean([0.4, 0.4, 0.2], base_materials_as_dict,
+                                                     'composition', 'fe3_o2')
+
+    assert mean == '7.92'
+
+
+def test_compute_mean_returns_none_when_not_all_values_are_filled():
+    first_material_as_dict = {
+        'composition': Composition(fe3_o2='1.2')
+    }
+    second_material_as_dict = {
+        'composition': Composition(fe3_o2=None)
+    }
+    third_material_as_dict = {
+        'composition': Composition(fe3_o2='30')
+    }
+    base_materials_as_dict = [first_material_as_dict, second_material_as_dict, third_material_as_dict]
+    mean = BlendingPropertiesCalculator.compute_mean([0.4, 0.4, 0.2], base_materials_as_dict,
+                                                     'composition', 'fe3_o2')
+
+    assert mean is None
