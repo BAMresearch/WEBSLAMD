8f573f6ccd86f019be35b15bc6af7a2fef20da7f
RugTehseen <tehseen.rug@iteratec.com>
2022-07-20 10:09:23 +0200

    slamd-25: tests for computation of blended costs

---
+++ tests/materials/processing/strategies/test_blended_properties_calculator.py
@@ -0,0 +1,32 @@
+from slamd.materials.processing.models.material import Costs
+from slamd.materials.processing.strategies.blending_properties_calculator import BlendingPropertiesCalculator
+
+
+def test_compute_blended_costs_correctly_computes_all_cost_properties_when_all_are_filled():
+    first_material_as_dict = {
+        'costs': Costs(co2_footprint=22.2, costs=77, delivery_time=80)
+    }
+    second_material_as_dict = {
+        'costs': Costs(co2_footprint=55.5, costs=99.2, delivery_time=11)
+    }
+    base_materials_as_dict = [first_material_as_dict, second_material_as_dict]
+    costs = BlendingPropertiesCalculator.compute_blended_costs([0.25, 0.75], base_materials_as_dict)
+
+    assert costs.co2_footprint == '47.17'
+    assert costs.costs == '93.65'
+    assert costs.delivery_time == '80.0'
+
+
+def test_compute_blended_costs_correctly_computes_all_cost_properties_when_not_all_are_filled():
+    first_material_as_dict = {
+        'costs': Costs(co2_footprint=22.2, costs=77, delivery_time=80)
+    }
+    second_material_as_dict = {
+        'costs': Costs(co2_footprint=55.5, costs='', delivery_time=11)
+    }
+    base_materials_as_dict = [first_material_as_dict, second_material_as_dict]
+    costs = BlendingPropertiesCalculator.compute_blended_costs([0.25, 0.75], base_materials_as_dict)
+
+    assert costs.co2_footprint == '47.17'
+    assert costs.costs is None
+    assert costs.delivery_time == '80.0'
