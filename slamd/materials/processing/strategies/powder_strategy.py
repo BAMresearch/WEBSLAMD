c2f2cbf1a31605c1eace4ebc22309af3afa74332
RugTehseen <tehseen.rug@iteratec.com>
2022-07-20 09:27:29 +0200

    slamd-25: move creation of additional properties to parent class

--- slamd/materials/processing/strategies/powder_strategy.py
+++ slamd/materials/processing/strategies/powder_strategy.py
@@ -1,7 +1,3 @@
-from functools import reduce
-
-from slamd.common.slamd_utils import string_to_number, numeric, not_numeric
-from slamd.materials.processing.models.additional_property import AdditionalProperty
 from slamd.materials.processing.models.powder import Powder, Composition, Structure
 from slamd.materials.processing.ratio_parser import RatioParser
 from slamd.materials.processing.strategies.base_material_strategy import MaterialStrategy
@@ -77,7 +73,7 @@ class PowderStrategy(MaterialStrategy):
         costs = self.compute_blended_costs(normalized_ratios, base_powders_as_dict)
         composition = self._compute_blended_composition(normalized_ratios, base_powders_as_dict)
         structure = self._compute_blended_structure(normalized_ratios, base_powders_as_dict)
-        additional_properties = self._compute_additional_properties(normalized_ratios, base_powders_as_dict)
+        additional_properties = self.compute_additional_properties(normalized_ratios, base_powders_as_dict)
 
         return Powder(type=base_powders_as_dict[0]['type'],
                       name=f'{blended_material_name}-{idx}',
@@ -116,75 +112,3 @@ class PowderStrategy(MaterialStrategy):
         blended_gravity = self.compute_mean(normalized_ratios, base_powders_as_dict, 'structure', 'gravity')
 
         return Structure(fine=blended_fine, gravity=blended_gravity)
-
-    def _compute_additional_properties(self, normalized_ratios, base_materials_as_dict):
-        additional_properties_for_all_base_materials = []
-        for base_powder in base_materials_as_dict:
-            additional_properties_for_all_base_materials.append(base_powder['additional_properties'])
-
-        properties_with_key_defined_in_all_base_materials = reduce(lambda x, y: self._keep_matching(x, y),
-                                                                   additional_properties_for_all_base_materials)
-        key_defined_in_all_base_materials = list(
-            map(lambda prop: prop.name, properties_with_key_defined_in_all_base_materials))
-
-        matching_properties_for_all_base_materials = []
-        for base_material_dict in base_materials_as_dict:
-            matching_properties_for_base_material = list(
-                filter(lambda prop: prop.name in key_defined_in_all_base_materials,
-                       base_material_dict['additional_properties']))
-
-            matching_properties_for_all_base_materials.append(matching_properties_for_base_material)
-
-        blended_additional_properties = []
-        for i in range(len(matching_properties_for_all_base_materials[0])):
-            ratios_with_property_values = zip(normalized_ratios, matching_properties_for_all_base_materials)
-
-            # Create list of property names together with their weigthed values and an information about the
-            # current ratio, e.g [('Prop1', 1.0, 0.5), ('Prop1', 2.0, 0.5)]
-            mapped_properties = list(map(lambda x: self.method_name(x[0], x[1][i]), ratios_with_property_values))
-            if numeric(mapped_properties[0][1]):
-                mean = sum(list(map(lambda x: x[1], mapped_properties)))
-                blended_additional_properties.append(
-                    AdditionalProperty(name=mapped_properties[0][0], value=str(round(mean, 2))))
-            else:
-                for item in mapped_properties:
-                    blended_property_names = [x.name for x in blended_additional_properties]
-                    if item[1] in blended_property_names:
-                        index_of_matching_name = blended_property_names.index(item[1])
-                        updated_value = float(blended_additional_properties[index_of_matching_name].value) + item[2]
-                        blended_additional_properties[index_of_matching_name].value = str(round(updated_value, 2))
-                    else:
-                        blended_additional_properties.append(
-                            AdditionalProperty(name=item[1], value=str(round(item[2], 2))))
-        return blended_additional_properties
-
-    def method_name(self, ratio, property):
-        return property.name, self._map_according_to_value(ratio, property.value), ratio
-
-    def _map_according_to_value(self, ratio, value):
-        if numeric(value):
-            return ratio * string_to_number(value)
-        return value
-
-    # we throw away all properties with keys (names) either not contained in additional_properties of all base materials
-    # or if the key is contained in additional_properties of all base materials but the types of the values are not
-    # matching as otherwise we cannot clearly separate continuous from categorical variables
-    def _keep_matching(self, first_property_list, second_property_list):
-        return [x for x in first_property_list if
-                self._is_contained_and_has_same_type_in_all_materials(x, second_property_list)]
-
-    def _is_contained_and_has_same_type_in_all_materials(self, prop, property_list):
-        matching_name = False
-        matching_type = False
-        names_of_properties = list(map(lambda p: p.name, property_list))
-        if prop.name in names_of_properties:
-            matching_name = True
-        if matching_name:
-            for additional_property in property_list:
-                if additional_property.name == prop.name:
-                    if (numeric(additional_property.value) and not_numeric(prop.value)) or (
-                            not_numeric(additional_property.value) and numeric(prop.value)):
-                        matching_type = False
-                    else:
-                        matching_type = True
-        return matching_name and matching_type
