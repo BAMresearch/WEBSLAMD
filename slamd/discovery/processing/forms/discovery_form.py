325efb91c7a864567a97147a31655ac4402f68ee
ghezal ahmad <ghezalahmad.zia@outlook.com>
2022-07-21 10:37:15 +0200

    upload File created

--- slamd/discovery/processing/forms/discovery_form.py
+++ slamd/discovery/processing/forms/discovery_form.py
@@ -1,8 +1,11 @@
 from flask_wtf import FlaskForm as Form
 from wtforms import validators, SelectMultipleField
+from wtforms import FileField
 
 
 class DiscoveryForm(Form):
+
+    upload_file = FileField('upload_file')
 
     input_features = SelectMultipleField(
         label='Input Features',
@@ -22,3 +25,6 @@ class DiscoveryForm(Form):
         choices=[]
     )
 
+    # left - exploit
+    # right - explore
+    # AI Model LOLO as a built-in
