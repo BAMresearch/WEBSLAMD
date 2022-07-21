a40a964346a83c16dc000e9884c8afd759e23d14
RugTehseen <tehseen.rug@iteratec.com>
2022-07-21 10:53:15 +0200

    slamd-50: basic packages and files for materials formulations

---
+++ slamd/formulations/processing/formulations_controller.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, render_template
+
+formulations = Blueprint('formulations', __name__,
+                         template_folder='../templates',
+                         static_folder='../static',
+                         static_url_path='static',
+                         url_prefix='/materials/formulations')
+
+
+@formulations.route('', methods=['GET'])
+def base_material_page():
+    return render_template('formulations.html')
---
+++ slamd/formulations/processing/formulations_service.py
@@ -0,0 +1,2 @@
+class FormulationsService:
+    pass
