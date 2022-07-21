0e72bd67f974ec4bfacd6a75c3c3807ae4d44046
ghezal ahmad <ghezalahmad.zia@outlook.com>
2022-07-21 13:21:34 +0200

    Model selection feild and Curiosity are created

--- slamd/discovery/processing/forms/discovery_form.py
+++ slamd/discovery/processing/forms/discovery_form.py
@@ -1,29 +1,40 @@
 from flask_wtf import FlaskForm as Form
-from wtforms import validators, SelectMultipleField
+from wtforms import validators, SelectMultipleField, SubmitField, SelectField, DecimalField
 from wtforms import FileField
+from wtforms.validators import NumberRange
 
 
 class DiscoveryForm(Form):
+    upload_file = FileField('')
+    submit = SubmitField('')
 
-    upload_file = FileField('upload_file')
+    select_model = SelectField(
+        label='Select Model',
+        validators=[validators.DataRequired()],
+        choices=['AI-Model (lolo Random Forrest', 'Statistics based model (Gaussian Process Regression)']
+    )
 
-
-    input_features = SelectMultipleField(
-        label='Input Features',
+    materials_data_input = SelectMultipleField(
+        label='Materials Data (Input)',
         validators=[validators.DataRequired()],
         choices=[]
     )
 
-    target_features = SelectMultipleField(
-        label='Target Features',
+    target_properties = SelectMultipleField(
+        label='Target Properties',
         validators=[validators.DataRequired()],
         choices=[]
     )
 
     a_priori_information = SelectMultipleField(
-        label='A-Priori Information',
+        label='A-priori Information',
         validators=[validators.DataRequired()],
         choices=[]
+    )
+
+    curiosity = DecimalField(
+        label='Curiosity (to control the weight of model uncertainty on predicted utility)',
+        validators=[NumberRange(min=0, max=10, message='bla')]
     )
 
     # left - exploit
