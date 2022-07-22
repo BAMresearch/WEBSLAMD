ab1c6ba0b2b0675219d3adc597544d67590726a4
RugTehseen <tehseen.rug@iteratec.com>
2022-07-22 11:44:41 +0200

    slamd-50: add constraint fields

--- slamd/formulations/processing/forms/formulations_min_max_form.py
+++ slamd/formulations/processing/forms/formulations_min_max_form.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm as Form
-from wtforms import FieldList, FormField, DecimalField, validators
+from wtforms import FieldList, FormField, DecimalField, validators, BooleanField
 from wtforms import StringField
 
 
@@ -29,5 +29,10 @@ class ProcessesEntriesForm(Form):
     )
 
 class FormulationsMinMaxForm(Form):
+
+    with_constraint = BooleanField(label='Do you want to set a weight constraint for formulations?')
+
+    weigth_constraint = DecimalField('Constraint (Sum of materials used for formulation) (kg)')
+
     materials_min_max_entries = FieldList(FormField(MaterialsMinMaxEntriesForm), min_entries=0)
     processes_entries = FieldList(FormField(ProcessesEntriesForm), min_entries=0)
