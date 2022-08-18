9b05e92146a7cd61e3ad9e31c08cab78720f5ff1
Horacio Scaffino <horacio.scaffino@iteratec.com>
2022-08-18 12:14:28 +0200

    Fix tooltip bug
    Dismissing the modal would leave the tooltip active. We can solve it changing the global configuration.
    
    The tooltips are now only active when hovering over them, not when they are focused.

--- slamd/static/global.js
+++ slamd/static/global.js
@@ -83,5 +83,5 @@ function fixInputValue(currentInputField) {
  */
  const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
  const tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-   return new bootstrap.Tooltip(tooltipTriggerEl)
+   return new bootstrap.Tooltip(tooltipTriggerEl, { trigger: "hover" })
  })
