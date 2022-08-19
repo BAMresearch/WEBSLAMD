2328b25a90b080016f501c8e7774665017ca27aa
Horacio Scaffino <horacio.scaffino@iteratec.com>
2022-08-19 11:45:30 +0200

    Simplify logic with a getCsrfToken custom command

--- cypress/e2e/test_blended_materials.cy.js
+++ cypress/e2e/test_blended_materials.cy.js
@@ -1,10 +1,9 @@
-describe("Test blended materials page", () => {
+describe("Test blending materials page", () => {
     beforeEach(() => {
         // Cypress starts out with a blank slate for each test
         // so we must tell it to visit our website with the `cy.visit()` command.
         // Since we want to visit the same URL at the start of all our tests,
         // we include it in our beforeEach function so that it runs before each test
-        cy.createPowders();
         cy.visit("http://localhost:5001/materials/blended");
     });
 
@@ -17,9 +16,21 @@ describe("Test blended materials page", () => {
         cy.findByLabelText("2 - Material type").select("Custom");
         cy.findByLabelText("2 - Material type").select("Powder");
     });
+});
 
-    it("Create blended powder", () => {
+describe("Test blending powders", () => {
+    beforeEach(() => {
+        // Cypress starts out with a blank slate for each test
+        // so we must tell it to visit our website with the `cy.visit()` command.
+        // Since we want to visit the same URL at the start of all our tests,
+        // we include it in our beforeEach function so that it runs before each test
+        cy.createExamplePowders();
+        cy.visit("http://localhost:5001/materials/blended");
         cy.findByText("Example Powder 1").should("exist");
         cy.findByText("Example Powder 2").should("exist");
+    });
+
+    it("Create blended powder", () => {
+
     });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,15 +1,26 @@
 // For more comprehensive examples of custom
 // commands please read more here:
 // https://on.cypress.io/custom-commands
+Cypress.Commands.add("getCsrfToken", () => {
+    cy.request("GET", "http://localhost:5001/materials/base").then((response) => {
+        // Cypress sets the session cookie automatically for us
+        // We need to parse the CSRF Token ourselves
+        const csrfTokenStart = response.body.split('<input id="csrf_token" name="csrf_token" type="hidden" value="', 2)[1];
+        const csrfToken = csrfTokenStart.split('"', 1)[0];
+        // Yield the csrfToken to the next command
+        cy.wrap(csrfToken);
+    });
+});
+
 Cypress.Commands.add("createBaseMaterial", (fixtureFilename, csrfToken) => {
     cy.fixture(fixtureFilename).then((materialData) => {
-        // Add the CSRF Token, otherwise we get a 400 Bad Request from the backend.
+        // Add the CSRF Token, otherwise we get a 400 Bad Request from the backend
         materialData.csrf_token = csrfToken;
         cy.request({
             url: "http://localhost:5001/materials/base",
             method: "POST",
             headers: {
-                // This is the smallest set of headers we need for the request to work.
+                // This is the smallest set of headers we need for the request to work
                 "Content-Type": "application/x-www-form-urlencoded",
             },
             body: materialData
@@ -17,17 +28,12 @@ Cypress.Commands.add("createBaseMaterial", (fixtureFilename, csrfToken) => {
     });
 });
 
-Cypress.Commands.add("createPowders", () => {
-    cy.request("GET", "http://localhost:5001/materials/base").then((response) => {
-        // Cypress sets the session cookie automatically for us
-        const csrfTokenStart = response.body.split('<input id="csrf_token" name="csrf_token" type="hidden" value="', 2)[1];
-        const csrfToken = csrfTokenStart.split('"', 1)[0];
-
+Cypress.Commands.add("createExamplePowders", () => {
+    cy.getCsrfToken().then((csrfToken) => {
         cy.createBaseMaterial("example_powder_1", csrfToken);
         cy.createBaseMaterial("example_powder_2", csrfToken);
     });
 });
-
 
 // Needed for Cypress testing library
 // https://testing-library.com/docs/cypress-testing-library/intro/
