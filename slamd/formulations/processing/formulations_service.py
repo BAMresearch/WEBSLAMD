faddfcccb7e6d8adecd8400a81bb5e3ab321c5be
RugTehseen <tehseen.rug@iteratec.com>
2022-08-24 12:51:03 +0200

    slamd-53: populate uuids in min max form for formulations

--- slamd/formulations/processing/formulations_service.py
+++ slamd/formulations/processing/formulations_service.py
@@ -44,17 +44,21 @@ class FormulationsService:
         liquid_names = [item['name'] for item in formulation_selection if item['type'] == 'Liquid']
         aggregates_names = [item['name'] for item in formulation_selection if item['type'] == 'Aggregates']
 
+        powder_uuids = [item['uuid'] for item in formulation_selection if item['type'] == 'Powder']
+        liquid_uuids = [item['uuid'] for item in formulation_selection if item['type'] == 'Liquid']
+        aggregates_uuids = [item['uuid'] for item in formulation_selection if item['type'] == 'Aggregates']
+
         # TODO: properly handle cases where e.g. no aggregates are specified
         if len(powder_names) == 0 or len(liquid_names) == 0 or len(aggregates_names) == 0:
             raise ValueNotSupportedException('You need to specify powders, liquids and aggregates')
 
         min_max_form = FormulationsMinMaxForm()
 
-        cls._create_min_max_form_entry(min_max_form.materials_min_max_entries,
+        cls._create_min_max_form_entry(min_max_form.materials_min_max_entries, ','.join(powder_uuids),
                                        'Powders ({0})'.format(', '.join(powder_names)), 'Powder')
-        cls._create_min_max_form_entry(min_max_form.materials_min_max_entries,
+        cls._create_min_max_form_entry(min_max_form.materials_min_max_entries, ','.join(liquid_uuids),
                                        'Liquids ({0})'.format(', '.join(liquid_names)), 'Liquid')
-        cls._create_min_max_form_entry(min_max_form.materials_min_max_entries,
+        cls._create_min_max_form_entry(min_max_form.materials_min_max_entries, ','.join(aggregates_uuids),
                                        'Aggregates ({0})'.format(', '.join(aggregates_names)), 'Aggregates')
 
         cls._create_non_editable_entries(formulation_selection, min_max_form, 'Admixture')
@@ -67,12 +71,13 @@ class FormulationsService:
     def _create_non_editable_entries(cls, formulation_selection, min_max_form, type):
         selection_for_type = [item for item in formulation_selection if item['type'] == type]
         for item in selection_for_type:
-            cls._create_min_max_form_entry(min_max_form.non_editable_entries, item['name'], type)
+            cls._create_min_max_form_entry(min_max_form.non_editable_entries, item['uuid'], item['name'], type)
 
     @classmethod
-    def _create_min_max_form_entry(cls, entries, name, type):
+    def _create_min_max_form_entry(cls, entries, uuids, name, type):
         entry = entries.append_entry()
         entry.materials_entry_name.data = name
+        entry.uuid_field.data = uuids
         entry.type_field.data = type
         if type == 'Powder' or type == 'Liquid' or type == 'Aggregates':
             entry.increment.name = type
