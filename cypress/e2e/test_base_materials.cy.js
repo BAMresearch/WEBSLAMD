94daff13e40ce1711dbd0802d4c22b0110bcf0e8
Michael Höpler <107104033+MiHoepler@users.noreply.github.com>
2022-08-29 18:05:29 +0200

    slamd-75: fixing cypress tests

--- cypress/e2e/test_base_materials.cy.js
+++ cypress/e2e/test_base_materials.cy.js
@@ -60,7 +60,7 @@ describe("Test creating base materials", () => {
     cy.findAllByLabelText("Value").last().type("Value 1");
 
     // Save material and check that it is listed
-    cy.findByText("6 - Save material").click();
+    cy.findByText("6 - Save material/process").click();
     cy.findByText("All base materials / processes").should("exist");
     cy.findByText("My Powder").should("exist");
   });
@@ -83,7 +83,7 @@ describe("Test creating base materials", () => {
     cy.findByLabelText("Relative Humidity (%)").type("34.5");
 
     // Save material and check that it is listed
-    cy.findByText("6 - Save material").click();
+    cy.findByText("6 - Save material/process").click();
     cy.findByText("All base materials / processes").should("exist");
     cy.findByText("My Process").should("exist");
   });
@@ -130,7 +130,7 @@ describe("Test creating base materials", () => {
     cy.findByText("Value 0").should("not.exist");
 
     // Save material and check that it is listed
-    cy.findByText("6 - Save material").click();
+    cy.findByText("6 - Save material/process").click();
     cy.findByText("All base materials / processes").should("exist");
     cy.findByText("My Custom Material").should("exist");
   });
@@ -173,7 +173,7 @@ describe("Test creating base materials", () => {
     cy.findByText("Value 0").should("not.exist");
 
     // Save material and check that it is listed
-    cy.findByText("6 - Save material").click();
+    cy.findByText("6 - Save material/process").click();
     cy.findByText("All base materials / processes").should("exist");
     cy.findByText("My Admixture").should("exist");
   });
