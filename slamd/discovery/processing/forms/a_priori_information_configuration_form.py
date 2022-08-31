eb9f38d0942e4c9faa0a149f9e185613ff1e71de
Horacio Scaffino <horacio.scaffino@iteratec.com>
2022-08-31 11:25:27 +0200

    Use DiscoveryForm instead of separate forms for the target configurations and a priori information.
    The field is defined exactly in the same way in DiscoveryForm. We can do without these files.

--- slamd/discovery/processing/forms/a_priori_information_configuration_form.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm as Form
-from wtforms import FieldList, FormField
-from slamd.discovery.processing.forms.field_configuration_form import FieldConfigurationForm
-
-
-class APrioriInformationConfigurationForm(Form):
-    a_priori_information_configurations = FieldList(FormField(FieldConfigurationForm),
-                                                    label='A priori information configurations',
-                                                    min_entries=0)
