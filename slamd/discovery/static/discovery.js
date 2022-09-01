2b68a587f55e216c6a9fe3ad3768ed3fcfd606f0
Michael Höpler <107104033+MiHoepler@users.noreply.github.com>
2022-09-01 11:33:28 +0200

    slamd-82: adding button toggle and description

--- slamd/discovery/static/discovery.js
+++ slamd/discovery/static/discovery.js
@@ -132,6 +132,7 @@ async function runExperiment() {
     target_configurations,
     a_priori_information_configurations,
   });
+  document.getElementById("download-experiment-button").disabled = false;
 }
 
 function toggleRunExperimentButton() {
