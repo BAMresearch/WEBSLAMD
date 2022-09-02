8247cc3f24b7f8acb0fc4f9539bbf81a3233b152
RugTehseen <tehseen.rug@iteratec.com>
2022-09-02 12:46:38 +0200

    slamd-80: add more tests to discovery controller

--- tests/discovery/processing/test_discovery_controller.py
+++ tests/discovery/processing/test_discovery_controller.py
@@ -1,4 +1,9 @@
+import json
+
+import pandas as pd
+
 from slamd import create_app
+from slamd.discovery.processing.add_targets_dto import DataWithTargetsDto, TargetDto
 from slamd.discovery.processing.discovery_service import DiscoveryService
 from slamd.discovery.processing.forms.upload_dataset_form import UploadDatasetForm
 from slamd.discovery.processing.models.dataset import Dataset
@@ -38,3 +43,57 @@ def test_slamd_creates_new_dataset_when_saving_is_successful(client, monkeypatch
     assert response.status_code == 302
     assert b'test dataset' not in response.data
     assert response.request.path == '/materials/discovery'
+
+
+def test_slamd_runs_experiment_and_shows_result(client, monkeypatch):
+    def mock_run_experiment(dataset_name, request):
+        data = {'feature': [1, 2], 'prediction': [3, 4]}
+        return pd.DataFrame.from_dict(data)
+
+    monkeypatch.setattr(DiscoveryService, 'run_experiment', mock_run_experiment)
+
+    response = client.post('/materials/discovery/test_dataset', data=b'{}')
+
+    assert response.status_code == 200
+
+    template = json.loads(response.data.decode('utf-8'))['template']
+    assert '<table ' in template
+    assert 'id="formulations_dataframe"' in template
+
+    assert '<th>feature</th>' in template
+    assert '<th>prediction</th>' in template
+
+    assert '<td>1</td>' in template
+    assert '<td>3</td>' in template
+
+    assert '<td>2</td>' in template
+    assert '<td>4</td>' in template
+
+
+def test_slamd_directs_to_add_targets_page(client, monkeypatch):
+    def mock_show_dataset_for_adding_targets(dataset_name):
+        df_data = {'feature1': [1], 'feature2': [2]}
+        all_dtos = [DataWithTargetsDto(0, 'feature1: 1, feature2: 2', [TargetDto(0, 'prediction', 11)])]
+        return pd.DataFrame.from_dict(df_data), all_dtos, ['prediction']
+
+    monkeypatch.setattr(DiscoveryService, 'show_dataset_for_adding_targets', mock_show_dataset_for_adding_targets)
+
+    response = client.get('/materials/discovery/test_dataset/add_targets', data=b'{}')
+
+    assert response.status_code == 200
+
+    template = response.data.decode('utf-8')
+    assert '<table ' in template
+    assert 'id="formulations_dataframe"' in template
+
+    assert '<th>feature1</th>' in template
+    assert '<th>feature2</th>' in template
+
+    assert '<td>1</td>' in template
+    assert '<td>2</td>' in template
+
+    assert 'feature1: 1, feature2: 2' in template
+    assert 'prediction' in template
+    assert '<input' in template
+    assert 'target-1-1' in template
+    assert '11' in template
