2f950d78a815920adcf2c8c31f8a0aaf1f828920
RugTehseen <tehseen.rug@iteratec.com>
2022-09-08 15:02:45 +0200

    slamd-targets-refactoring: earlier check of too many weights

--- slamd/formulations/processing/weight_input_preprocessor.py
+++ slamd/formulations/processing/weight_input_preprocessor.py
@@ -1,5 +1,7 @@
-from slamd.common.slamd_utils import empty
+from slamd.common.error_handling import SlamdRequestTooLargeException
 from slamd.materials.processing.materials_facade import MaterialsFacade
+
+MAX_NUMBER_OF_WEIGHTS = 10000
 
 
 class WeightInputPreprocessor:
@@ -14,11 +16,18 @@ class WeightInputPreprocessor:
     @classmethod
     def collect_weights(cls, formulation_config):
         all_materials_weights = []
+        total_number_of_weight_combinations = 1
         for i, entry in enumerate(formulation_config):
             if i == len(formulation_config) - 1:
                 continue
             weights_for_material = cls._create_weights_for_material(entry)
             all_materials_weights.append(weights_for_material)
+
+            total_number_of_weight_combinations *= len(weights_for_material)
+
+            if total_number_of_weight_combinations >= MAX_NUMBER_OF_WEIGHTS:
+                raise SlamdRequestTooLargeException(
+                    f'Too many weights were requested. At most {MAX_NUMBER_OF_WEIGHTS} weights can be created!')
 
         return all_materials_weights
 
