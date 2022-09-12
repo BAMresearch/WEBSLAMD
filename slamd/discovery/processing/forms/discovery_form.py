ab4477c990e1793c48d37c6e8ed17e267b896302
Horacio Scaffino <horacio.scaffino@iteratec.com>
2022-09-12 16:39:17 +0200

    Add asterisks to discovery form fields

--- slamd/discovery/processing/forms/discovery_form.py
+++ slamd/discovery/processing/forms/discovery_form.py
@@ -5,28 +5,28 @@ from slamd.discovery.processing.forms.field_configuration_form import FieldConfi
 
 class DiscoveryForm(Form):
     materials_data_input = SelectMultipleField(
-        label='Materials Data (Input)',
+        label='Materials Data (Input) (1 at least)',
         validators=[validators.DataRequired(message='Select at least one column of the dataset as input')],
         choices=[],
         render_kw={'style': 'height:120px'}
     )
 
     target_properties = SelectMultipleField(
-        label='Target Properties',
+        label='Target Properties (1 at least)',
         validators=[validators.DataRequired(message='Select at least one column of the dataset as target')],
         choices=[],
         render_kw={'style': 'height:120px'}
     )
 
     a_priori_information = SelectMultipleField(
-        label='A priori Information',
+        label='A priori Information (opt.)',
         validators=[validators.Optional()],
         choices=[],
         render_kw={'style': 'height:120px'}
     )
 
     model = SelectField(
-        label='Select Model',
+        label='Select Model *',
         validators=[validators.DataRequired(message='Model cannot be empty')],
         choices=[
             'Statistics-based model (Gaussian Process Regression)',
@@ -35,10 +35,12 @@ class DiscoveryForm(Form):
     )
 
     curiosity = DecimalRangeField(
-        label='Curiosity (to control the weight of model uncertainty on predicted utility)',
+        label='Curiosity (to control the weight of model uncertainty on predicted utility) *',
         default=1.00,
         places=2,
-        validators=[validators.NumberRange(min=0, max=10, message='The curiosity value should be between 0 and 10')]
+        validators=[
+            validators.NumberRange(min=0, max=10, message='The curiosity value should be between 0 and 10')
+        ]
     )
 
     target_configurations = FieldList(FormField(FieldConfigurationForm),
