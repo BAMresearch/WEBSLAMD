ac7698739ce22e5c37af3deb4efe93d3aeb23fa7
Horacio Scaffino <horacio.scaffino@iteratec.com>
2022-09-12 16:36:12 +0200

    Clarify what is required in the labels

--- slamd/formulations/processing/forms/materials_and_processes_selection_form.py
+++ slamd/formulations/processing/forms/materials_and_processes_selection_form.py
@@ -5,48 +5,48 @@ from wtforms import validators, SelectMultipleField, SubmitField, DecimalField, 
 class MaterialsAndProcessesSelectionForm(Form):
 
     powder_selection = SelectMultipleField(
-        label='1.1 - Powders',
+        label='1.1 - Powders (1 at least)',
         validators=[validators.DataRequired(message='Select at least one powder')],
         choices=[]
     )
 
     liquid_selection = SelectMultipleField(
-        label='1.2 - Liquids',
+        label='1.2 - Liquids (1 at least)',
         validators=[validators.DataRequired(message='Select at least one liquid')],
         choices=[]
     )
 
     aggregates_selection = SelectMultipleField(
-        label='1.3 - Aggregates',
+        label='1.3 - Aggregates (1 at least)',
         validators=[validators.DataRequired(message='Select at least one aggregate')],
         choices=[]
     )
 
     admixture_selection = SelectMultipleField(
-        label='1.4 - Admixture',
+        label='1.4 - Admixture (opt.)',
         validators=[validators.Optional()],
         choices=[]
     )
 
     custom_selection = SelectMultipleField(
-        label='1.5 - Custom',
+        label='1.5 - Custom (opt.)',
         validators=[validators.Optional()],
         choices=[]
     )
 
     process_selection = SelectMultipleField(
-        label='1.6 - Processes',
+        label='1.6 - Processes (opt.)',
         validators=[validators.Optional()],
         choices=[]
     )
 
     weight_constraint = DecimalField(
-        label='1.7 - Constraint (Sum of materials used for formulation) (kg)',
+        label='1.7 - Constraint (Sum of materials used for formulation) (kg) *',
         validators=[validators.DataRequired(message='Weight constraint cannot be empty')]
     )
 
     dataset_name = StringField(
-        label='1.8 - Name of the dataset',
+        label='1.8 - Name of the dataset *',
         validators=[validators.DataRequired(message='Dataset name cannot be empty')]
     )
 
