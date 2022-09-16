df5ae58c6c6656b4c53f9e07a12655c5cdc87dd3
Horacio Scaffino <horacio.scaffino@iteratec.com>
2022-09-16 15:19:25 +0200

    Add an interactive plot side by side with the old one
    We will remove the old one once we get the right output.

--- slamd/discovery/processing/algorithms/plot_generator.py
+++ slamd/discovery/processing/algorithms/plot_generator.py
@@ -1,7 +1,10 @@
+import json
 import base64
 from io import BytesIO
 
 from matplotlib import pyplot as plt
+import plotly
+import plotly.express as px
 import seaborn as sns
 
 
@@ -9,15 +12,24 @@ class PlotGenerator:
 
     @classmethod
     def create_target_scatter_plot(cls, target_list):
+        static_plot = cls._create_static_plot(target_list)
+        interactive_plot = cls._create_interactive_plot(target_list)
+        return static_plot, interactive_plot
+
+    @classmethod
+    def _create_static_plot(cls, target_list):
         grid = sns.PairGrid(target_list, diag_sharey=False, corner=True, hue="Utility")
         grid.map_diag(sns.histplot, hue=None, color=".3")
         grid.map_lower(sns.scatterplot)
         grid.add_legend()
         plt.plot()
-
         img_bytes = BytesIO()
         plt.savefig(img_bytes, format='png')
         plt.close()
         img_bytes.seek(0)
-
         return base64.b64encode(img_bytes.getvalue()).decode()
+
+    @classmethod
+    def _create_interactive_plot(cls, target_list):
+        fig = px.scatter_matrix(target_list)
+        return json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
