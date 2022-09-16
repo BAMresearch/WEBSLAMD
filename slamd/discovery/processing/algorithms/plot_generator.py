3c6fabba306f3d95b1cdf0026ba7f6dd5159ade5
Horacio Scaffino <horacio.scaffino@iteratec.com>
2022-09-16 16:33:48 +0200

    Add title, hide diagonal and upper half of plot, fix the height.

--- slamd/discovery/processing/algorithms/plot_generator.py
+++ slamd/discovery/processing/algorithms/plot_generator.py
@@ -32,5 +32,10 @@ class PlotGenerator:
     @classmethod
     def _create_interactive_plot(cls, target_list):
         dimensions = [col for col in target_list.columns if col != "Utility"]
-        fig = px.scatter_matrix(target_list, dimensions=dimensions, color="Utility")
+        fig = px.scatter_matrix(target_list, dimensions=dimensions, color="Utility",
+                                title='Scatter matrix of target properties')
+        fig.update_traces(diagonal_visible=False, showupperhalf=False)
+        fig.update_layout(
+            height=1000,
+        )
         return json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
