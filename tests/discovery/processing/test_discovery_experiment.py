73dab069baabe2315407a155af11e3f6ca3aa570
Stefan Lueders <stefan.lueders@iteratec.com>
2022-09-19 14:33:09 +0200

    slamd-94b: Fixed apriori filter unit test

--- tests/discovery/processing/test_discovery_experiment.py
+++ tests/discovery/processing/test_discovery_experiment.py
@@ -74,19 +74,20 @@ def test_filter_apriori_thresholds():
     experiment = DiscoveryExperiment(
         pd.DataFrame({'x': [], 'y': [], 'z': [], 'u': [], 'v': [], 'w': []}), model="", curiosity=1, features=[],
         targets=['x', 'y'], target_weights=[1, 1], target_thresholds=[None, None], target_max_or_min=['max', 'min'],
-        apriori_columns=['u', 'v'], apriori_thresholds=[3, 6], apriori_weights=[1, 1], apriori_max_or_min=['max', 'min']
+        apriori_columns=['u', 'v'], apriori_thresholds=[4, 5], apriori_weights=[1, 1], apriori_max_or_min=['max', 'min']
     )
 
     df = experiment.dataframe
     df['u'] = [1, 2, 3, 4, 5, 6, 7, 8]
     df['v'] = [1, 2, 3, 4, 5, 6, 7, 8]
-    df['x'] = [np.nan, np.nan,      np.nan, 4,      np.nan, np.nan, 7,      8]
-    df['y'] = [1,      np.nan, np.nan, np.nan, np.nan, np.nan,      np.nan, 8]
+    df['x'] = [np.nan, np.nan, np.nan, 4, np.nan, np.nan, 7, 8]
+    df['y'] = [1, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, 8]
+
 
     result = experiment.filter_apriori_with_thresholds(df)
 
-    assert result['u'].values.all() == np.array([1, 4, 5, 7, 8]).all()
-    assert result['v'].values.all() == np.array([1, 4, 5, 7, 8]).all()
-    assert result['x'].values.all() == np.array([1, 4, 5, 7, 8]).all()
-    assert result['y'].values.all() == np.array([1, 4, 5, 7, 8]).all()
+    assert np.array_equal(result['u'].values, np.array([1, 4, 5, 7, 8]))
+    assert np.array_equal(result['v'].values, np.array([1, 4, 5, 7, 8]))
+    assert np.array_equal(np.nan_to_num(result['x'].values), np.array([0, 4, 0, 7, 8]))
+    assert np.array_equal(np.nan_to_num(result['y'].values), np.array([1, 0, 0, 0, 8]))
 
