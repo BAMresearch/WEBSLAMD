f82a9cffd128d947ae580fca1200f3712b121ea7
Stefan Lueders <stefan.lueders@iteratec.com>
2022-09-19 13:48:40 +0200

    slamd-94b: Added unit test for filter_apriori_thresholds

--- tests/discovery/processing/test_discovery_experiment.py
+++ tests/discovery/processing/test_discovery_experiment.py
@@ -3,7 +3,6 @@ import numpy as np
 
 from slamd.discovery.processing.algorithms.discovery_experiment import DiscoveryExperiment
 
-
 def test_clip_predictions_for_one_target_no_threshold():
     experiment = DiscoveryExperiment(
         pd.DataFrame({'x': []}), model="", curiosity=1, features=[], targets=['x'], target_weights=[1],
@@ -14,11 +13,9 @@ def test_clip_predictions_for_one_target_no_threshold():
 
     input_prediction = np.arange(10)
     experiment.prediction = input_prediction
-
     clipped_prediction = experiment.clip_predictions()
 
     assert clipped_prediction.all() == input_prediction.all()
-
 
 def test_clip_predictions_for_one_target_min_threshold():
     experiment = DiscoveryExperiment(
@@ -30,11 +27,9 @@ def test_clip_predictions_for_one_target_min_threshold():
 
     input_prediction = np.arange(10)
     experiment.prediction = input_prediction
-
     clipped_prediction = experiment.clip_predictions()
 
     assert clipped_prediction.all() == np.array([5, 5, 5, 5, 5, 5, 6, 7, 8, 9, 10]).all()
-
 
 def test_clip_predictions_for_one_target_max_threshold():
     experiment = DiscoveryExperiment(
@@ -46,11 +41,9 @@ def test_clip_predictions_for_one_target_max_threshold():
 
     input_prediction = np.arange(10)
     experiment.prediction = input_prediction
-
     clipped_prediction = experiment.clip_predictions()
 
     assert clipped_prediction.all() == np.array([0, 1, 2, 3, 4, 5, 5, 5, 5, 5]).all()
-
 
 def test_clip_predictions_for_two_targets_maxmin_threshold():
     experiment = DiscoveryExperiment(
@@ -73,8 +66,27 @@ def test_clip_predictions_for_two_targets_maxmin_threshold():
         ]
     )
     experiment.prediction = input_prediction
-
     clipped_prediction = experiment.clip_predictions()
 
     assert clipped_prediction.all() == expected_output.all()
+
+def test_filter_apriori_thresholds():
+    experiment = DiscoveryExperiment(
+        pd.DataFrame({'x': [], 'y': [], 'z': [], 'u': [], 'v': [], 'w': []}), model="", curiosity=1, features=[],
+        targets=['x', 'y'], target_weights=[1, 1], target_thresholds=[None, None], target_max_or_min=['max', 'min'],
+        apriori_columns=['u', 'v'], apriori_thresholds=[3, 6], apriori_weights=[1, 1], apriori_max_or_min=['max', 'min']
+    )
+
+    df = experiment.dataframe
+    df['u'] = [1, 2, 3, 4, 5, 6, 7, 8]
+    df['v'] = [1, 2, 3, 4, 5, 6, 7, 8]
+    df['x'] = [np.nan, np.nan,      np.nan, 4,      np.nan, np.nan, 7,      8]
+    df['y'] = [1,      np.nan, np.nan, np.nan, np.nan, np.nan,      np.nan, 8]
+
+    result = experiment.filter_apriori_with_thresholds(df)
+
+    assert result['u'].values.all() == np.array([1, 4, 5, 7, 8]).all()
+    assert result['v'].values.all() == np.array([1, 4, 5, 7, 8]).all()
+    assert result['x'].values.all() == np.array([1, 4, 5, 7, 8]).all()
+    assert result['y'].values.all() == np.array([1, 4, 5, 7, 8]).all()
 
