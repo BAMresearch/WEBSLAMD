0dfab0db6e6d94ee50c6fa89d0a8cc67a9c4673c
Horacio Scaffino <horacio.scaffino@iteratec.com>
2022-09-20 13:36:53 +0200

    Add comment

--- slamd/discovery/processing/algorithms/plot_generator.py
+++ slamd/discovery/processing/algorithms/plot_generator.py
@@ -10,14 +10,15 @@ class PlotGenerator:
         dimensions = [col for col in target_list.columns if col != 'Utility']
         if len(dimensions) == 1:
             # Generate a simple scatter plot if there is only one target property.
+            # We include the Utility color-coded for aesthetic reasons.
             fig = px.scatter(target_list, x=dimensions[0], y='Utility', color='Utility',
                              title='Scatter plot of target properties')
             fig.update_traces(hovertemplate='X: %{x:.2f}, Y: %{y:.2f}')
         elif len(dimensions) == 2:
             # Plotly 5.10 issue: px.scatter_matrix() does not output anything when the matrix is 1x1.
             # We need to handle this case separately and generate a single scatter plot.
-            fig = px.scatter(target_list, x=dimensions[0], y=dimensions[1],
-                             color='Utility', title='Scatter plot of target properties')
+            fig = px.scatter(target_list, x=dimensions[0], y=dimensions[1], color='Utility',
+                             title='Scatter plot of target properties')
             fig.update_traces(hovertemplate='X: %{x:.2f}, Y: %{y:.2f}, Utility: %{marker.color:.2f}')
         else:
             # General case
