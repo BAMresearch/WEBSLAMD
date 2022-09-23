35f9a0e2a1c0ddadfa3d505b53f3dfbf2cfa532c
Horacio Scaffino <horacio.scaffino@iteratec.com>
2022-09-23 12:00:27 +0200

    Use the same tooltips for all plots

--- slamd/discovery/processing/algorithms/plot_generator.py
+++ slamd/discovery/processing/algorithms/plot_generator.py
@@ -14,27 +14,21 @@ class PlotGenerator:
             # We include the Utility color-coded for aesthetic reasons.
             fig = px.scatter(plot_df, x=dimensions[0], y='Utility', color='Utility',
                              custom_data=['Row number'], title='Scatter plot of target properties')
-            fig.update_traces(
-                hovertemplate='Row number: %{customdata}, X: %{x:.2f}, Y: %{y:.2f}'
-            )
         elif len(dimensions) == 2:
             # Plotly 5.10 issue: px.scatter_matrix() does not output anything when the matrix is 1x1.
             # We need to handle this case separately and generate a single scatter plot.
             fig = px.scatter(plot_df, x=dimensions[0], y=dimensions[1], color='Utility',
                              custom_data=['Row number'], title='Scatter plot of target properties')
-            fig.update_traces(
-                hovertemplate='Row number: %{customdata}, X: %{x:.2f}, Y: %{y:.2f}, Utility: %{marker.color:.2f}'
-            )
         else:
             # General case
             fig = px.scatter_matrix(plot_df, dimensions=dimensions, color='Utility',
                                     custom_data=['Row number'], title='Scatter matrix of target properties')
             # Format the tooltips in a generic way good enough for all subplots
-            fig.update_traces(
-                diagonal_visible=False, showupperhalf=False,
-                hovertemplate='Row number: %{customdata}, X: %{x:.2f}, Y: %{y:.2f}, Utility: %{marker.color:.2f}'
-            )
+            fig.update_traces(diagonal_visible=False, showupperhalf=False)
 
+        fig.update_traces(
+            hovertemplate='Row number: %{customdata}, X: %{x:.2f}, Y: %{y:.2f}, Utility: %{marker.color:.2f}'
+        )
         fig.update_layout(height=1000)
         return json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
 
