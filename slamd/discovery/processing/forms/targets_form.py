39ce8fd0e192999cf7d5490134b581f6dc8d1b4f
Stefan Lueders <stefan.lueders@iteratec.com>
2022-09-23 09:19:40 +0200

    slamd-95b: Minor wording and padding adjustments, centered navbar and outlined burger button

--- slamd/discovery/processing/forms/targets_form.py
+++ slamd/discovery/processing/forms/targets_form.py
@@ -5,12 +5,12 @@ from wtforms import SubmitField, StringField, validators, SelectMultipleField
 class TargetsForm(Form):
 
     target_value = StringField(
-        label='1.1 Add new target',
+        label='1.1 Either add a new target column...',
         validators=[validators.DataRequired(message='Name cannot be empty')],
     )
 
     choose_target_field = SelectMultipleField(
-        label='1.2 Choose existing target for adding labels',
+        label='1.2 ...or choose an existing column to edit',
         validators=[validators.DataRequired(message='Target cannot be empty')],
         choices=[]
     )
