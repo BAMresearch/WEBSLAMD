870c197451dae2182a5a45e8f3f0c69749bfa47a
Stefan Lueders <stefan.lueders@iteratec.com>
2022-09-28 12:12:24 +0200

    slamd-96: Refactored _filter_apriori..., added _validate_experiment, completing preliminary ExperimentPreprocessor

--- slamd/discovery/processing/algorithms/experiment_preprocessor.py
+++ slamd/discovery/processing/algorithms/experiment_preprocessor.py
@@ -4,13 +4,20 @@ from slamd.common.error_handling import SequentialLearningException
 class ExperimentPreprocessor:
     @classmethod
     def preprocess(cls, exp):
-        if len(exp.target_names) == 0:
-            raise SequentialLearningException('No targets were specified!')
-
+        cls._validate_experiment(exp)
+        cls._filter_apriori_with_thresholds(exp)
         cls._encode_categoricals(exp)
         cls._filter_missing_inputs(exp)
         cls._decide_max_or_min(exp)
 
+    @classmethod
+    def _validate_experiment(cls, exp):
+        # TODO Think of more sensible errors we could throw
+        if len(exp.target_names) == 0:
+            raise SequentialLearningException('No targets were specified!')
+        for value in exp.target_max_or_min + exp.apriori_max_or_min:
+            if value not in ['min', 'max']:
+                raise SequentialLearningException(f'Invalid value for max_or_min, got {value}')
 
     @classmethod
     def _encode_categoricals(cls, exp):
@@ -32,10 +39,27 @@ class ExperimentPreprocessor:
         # TODO find a way to do this non-destructively
         # Multiply the column by -1 if it needs to be minimized
         for (column, value) in zip(exp.target_names+exp.apriori_names, exp.target_max_or_min+exp.apriori_max_or_min):
-            if value not in ['min', 'max']:
-                raise SequentialLearningException(f'Invalid value for max_or_min, got {value}')
             if value == 'min':
                 exp.dataframe[column] *= (-1)
+
+    @classmethod
+    def _filter_apriori_with_thresholds(cls, exp):
+        # TODO This function could be handled "live" and non-destructively in label_index and nolabel_index
+        for (column, value, threshold) in zip(exp.apriori_names, exp.apriori_max_or_min, exp.apriori_thresholds):
+            if threshold is None:
+                continue
+
+            # index of rows in which all target columns are nan
+            nodata_index = exp.targets_df.isna().all(axis=1)
+            if value == 'max':
+                # Get dataframe mask based on threshold value and nodata_index.
+                # Apply mask to dataframe. Get index of values to drop.
+                # Use new index to drop values from original dataframe.
+                exp.dataframe.drop(exp.dataframe[(exp.dataframe[column] < threshold) & nodata_index].index, inplace=True)
+            else:
+                exp.dataframe.drop(exp.dataframe[(exp.dataframe[column] > threshold) & nodata_index].index, inplace=True)
+
+        exp.dataframe.reset_index(drop=True, inplace=True)
 
 
 
@@ -60,27 +84,6 @@ class ExperimentPreprocessor:
         #
         # std = self.apriori_df.std().apply(lambda x: x if x != 0 else 1)
         # self.apriori_df = (self.apriori_df - self.apriori_df.mean()) / std
-
-
-
-    def filter_apriori_with_thresholds(self, df):
-        for (column, value, threshold) in zip(self.apriori_columns, self.apriori_max_or_min, self.apriori_thresholds):
-            if value not in ['min', 'max']:
-                raise SequentialLearningException(f'Invalid value for max_or_min, got {value}')
-            if threshold is None:
-                continue
-
-            # index of rows in which all target columns are nan
-            nodata_index = df[self.targets].isna().all(axis=1)
-            if value == 'max':
-                # Get dataframe mask based on threshold value and nodata_index.
-                # Apply mask to dataframe. Get index of values to drop.
-                # Use new index to drop values from original dataframe.
-                df.drop(df[(df[column] < threshold) & nodata_index].index, inplace=True)
-            else:
-                df.drop(df[(df[column] > threshold) & nodata_index].index, inplace=True)
-
-        return df.reset_index(drop=True)
 
 
 
