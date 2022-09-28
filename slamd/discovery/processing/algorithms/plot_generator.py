6a6c6479b562021ef5eb656c1b99437c90bde79f
Horacio Scaffino <horacio.scaffino@iteratec.com>
2022-09-28 09:14:41 +0200

    Update comment

--- slamd/discovery/processing/algorithms/plot_generator.py
+++ slamd/discovery/processing/algorithms/plot_generator.py
@@ -35,8 +35,8 @@ class PlotGenerator:
 
     @classmethod
     def create_tsne_input_space_plot(cls, plot_df):
-        # The perplexity cannot be bigger than the number of data points (the length of the dataframe).
-        # Prevent this error case by picking the smallest of the two.
+        # The perplexity must be less than the number of data points (the length of the dataframe).
+        # Handle this edge case by picking the smallest of the two.
         tsne = TSNE(n_components=2, verbose=1, perplexity=min(40, len(plot_df) - 1), n_iter=300, random_state=1000)
         # Exclude the columns that do not belong to the features
         tsne_result = tsne.fit_transform(plot_df.drop(columns=['Row number', 'Utility', 'is_train_data']))
