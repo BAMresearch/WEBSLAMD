53f632b0c65fa832fa2c9388a0319ef99bf82bb3
Stefan Lueders <stefan.lueders@iteratec.com>
2022-10-04 14:42:14 +0200

    slamd-96: Minor refactor in experiment_postprocessor.py, mostly formatting

--- slamd/discovery/processing/algorithms/experiment_postprocessor.py
+++ slamd/discovery/processing/algorithms/experiment_postprocessor.py
@@ -8,37 +8,39 @@ class ExperimentPostprocessor:
     @classmethod
     def postprocess(cls, exp):
         # Construct dataframe for output
+
         df = exp.orig_data.loc[exp.nolabel_index].copy()
         # Add the columns with utility and novelty values
-        df['Utility'] = exp.utility
-        df['Utility'] = df['Utility'].round(6)
+        df['Utility'] = exp.utility.round(6)
         df['Novelty'] = exp.novelty.round(6)
-        # df = df.iloc[self.prediction_index].assign(Utility=pd.Series(utility_function).values)
-        # df = df.loc[self.prediction_index].assign(Novelty=pd.Series(novelty_factor).values)
 
-        # TODO prediction index mismatch
         for target in exp.target_names:
-            df.loc[exp.nolabel_index, target] = exp.prediction[target].values
-            df[target] = df[target].round(6)
-            df[f'Uncertainty ({target})'] = exp.uncertainty[target].values
-            df[f'Uncertainty ({target})'] = df[f'Uncertainty ({target})'].round(5)
+            df.loc[exp.nolabel_index, target] = exp.prediction[target].round(6)
+            df[f'Uncertainty ({target})'] = exp.uncertainty[target].round(5)
 
         df = cls.preprocess_dataframe_for_output_table(df, exp)
         scatter_plot = cls.plot_output_space(df, exp)
         tsne_plot = cls.plot_input_space(exp.utility, exp)
+
         return df, scatter_plot, tsne_plot
 
     @classmethod
-    def move_after_row_column(cls, df, cols_to_move=[]):
+    def move_after_row_column(cls, df, cols_to_move=None):
         """
         Move one or several columns after the column named 'Row number'.
         """
         cols = df.columns.tolist()
         seg1 = cols[:list(cols).index('Row number') + 1]
-        seg2 = cols_to_move
+
+        if cols_to_move is None:
+            seg2 = []
+        else:
+            seg2 = cols_to_move
+
         # Make sure to remove overlapping elements between the segments
         seg1 = [i for i in seg1 if i not in seg2]
         seg3 = [i for i in cols if i not in seg1 + seg2]
+
         # Return a new dataset with the columns in the new order to be assigned to the same variable.
         return df[seg1 + seg2 + seg3]
 
@@ -61,20 +63,26 @@ class ExperimentPostprocessor:
         columns_for_plot = exp.target_names.copy()
         columns_for_plot.extend([f'Uncertainty ({target})' for target in exp.target_names])
         columns_for_plot.extend(['Utility', 'Row number'])
+
         if len(exp.apriori_names) > 0:
             columns_for_plot.extend(exp.apriori_names)
+
         return PlotGenerator.create_target_scatter_plot(df[columns_for_plot])
 
     @classmethod
     def plot_input_space(cls, utility_function, exp):
         plot_df = exp.features_df.copy()
+
         plot_df['is_train_data'] = 'Predicted'
-        plot_df['is_train_data'].iloc[exp.label_index] = 'Labelled'
+        plot_df.loc[exp.label_index, 'is_train_data'] = 'Labelled'
+
         plot_df['Utility'] = -np.inf
-        plot_df['Utility'].iloc[exp.nolabel_index] = pd.Series(utility_function).values
+        plot_df.loc[exp.nolabel_index, 'Utility'] = pd.Series(utility_function).values
         plot_df = plot_df.sort_values(by='Utility', ascending=False)
+
         # Number the rows from 1 to n (length of the dataframe) to identify them easier on the plots.
         plot_df.insert(loc=0, column='Row number', value=[i for i in range(1, len(plot_df) + 1)])
+
         return PlotGenerator.create_tsne_input_space_plot(plot_df)
 
 
