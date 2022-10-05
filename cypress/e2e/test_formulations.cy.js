b75e43a919f9f0ee99fca39bb2268af30c452ccf
Horacio Scaffino <horacio.scaffino@iteratec.com>
2022-10-05 13:57:18 +0200

    Add test with 2 powders, 2 liquids, 2 aggregates

--- cypress/e2e/test_formulations.cy.js
+++ cypress/e2e/test_formulations.cy.js
@@ -63,4 +63,65 @@ describe("Test materials formulations form", () => {
     // Submit dataset with the default dataset name
     cy.findByText("Submit").click();
   });
+
+  it("Create formulations with two of every required material", () => {
+    cy.findByLabelText("1.1 - Powders (select one at least)").select(["Example Powder 1", "Example Powder 2"]);
+    cy.findByLabelText("1.2 - Liquids (select one at least)").select(["Example Liquid 1", "Example Liquid 2"]);
+    cy.findByLabelText("1.3 - Aggregates (select one at least)").select([
+      "Example Aggregates 1",
+      "Example Aggregates 2",
+    ]);
+    cy.findByLabelText("1.7 - Constraint (Sum of materials used for formulation) (kg) *")
+      .type("100")
+      .should("have.value", "100");
+
+    cy.findByText("4 - Configure weights for each material type").click();
+    // Wait for the modal animation to finish
+    cy.wait(400);
+    cy.findByText("Change Selection").should("exist");
+    cy.findByText("Do you really want to change the chosen selection?").should("exist");
+    cy.findByText("Close").should("exist");
+    cy.findByText("Confirm").click();
+
+    // Fill in the increment, min, max values
+    cy.findAllByLabelText("Increment (kg)").first().type(40).should("have.value", 40);
+    cy.findAllByLabelText("Min (kg)").first().type(20).should("have.value", 20);
+    cy.findAllByLabelText("Max (kg)").first().type(60).should("have.value", 60);
+    cy.findAllByLabelText("Increment (kg)").eq(1).type(10).should("have.value", 10);
+    cy.findAllByLabelText("Min (kg)").eq(1).type(10).should("have.value", 10);
+    cy.findAllByLabelText("Max (kg)").eq(1).type(10).should("have.value", 10);
+    // Check the autocompletion feature
+    cy.findAllByLabelText("Min (kg)").last().should("have.value", "70.00");
+    cy.findAllByLabelText("Max (kg)").last().should("have.value", "30.00");
+    cy.findByText("5 - Show mixture in terms of base material composition").click().scrollIntoView();
+
+    // Check that the configurations were generated correctly
+    cy.findByDisplayValue("20.0/10.0/70.0").should("exist");
+    cy.findByDisplayValue("60.0/10.0/30.0").should("exist");
+    cy.findByText("6 - Create material formulations for given configuration").click();
+
+    // Check that the materials formulations were generated correctly
+    cy.findByText("Show / hide formulations").scrollIntoView();
+    cy.findByText("Idx_Sample").should("exist");
+    // Check the first 4 columns for every row
+    cy.findByText(0).next().expectFloatToEqual(20.0).next().expectFloatToEqual(10.0).next().expectFloatToEqual(70.0);
+    cy.findByText(1).next().expectFloatToEqual(60.0).next().expectFloatToEqual(10.0).next().expectFloatToEqual(30.0);
+    cy.findByText(2).next().expectFloatToEqual(20.0).next().expectFloatToEqual(10.0).next().expectFloatToEqual(70.0);
+    cy.findByText(3).next().expectFloatToEqual(60.0).next().expectFloatToEqual(10.0).next().expectFloatToEqual(30.0);
+    cy.findByText(4).next().expectFloatToEqual(20.0).next().expectFloatToEqual(10.0).next().expectFloatToEqual(70.0);
+    cy.findByText(5).next().expectFloatToEqual(60.0).next().expectFloatToEqual(10.0).next().expectFloatToEqual(30.0);
+    cy.findByText(6).next().expectFloatToEqual(20.0).next().expectFloatToEqual(10.0).next().expectFloatToEqual(70.0);
+    cy.findByText(7).next().expectFloatToEqual(60.0).next().expectFloatToEqual(10.0).next().expectFloatToEqual(30.0);
+    cy.findByText(8).next().expectFloatToEqual(20.0).next().expectFloatToEqual(10.0).next().expectFloatToEqual(70.0);
+    cy.findByText(9).next().expectFloatToEqual(60.0).next().expectFloatToEqual(10.0).next().expectFloatToEqual(30.0);
+    cy.findByText(10).next().expectFloatToEqual(20.0).next().expectFloatToEqual(10.0).next().expectFloatToEqual(70.0);
+    cy.findByText(11).next().expectFloatToEqual(60.0).next().expectFloatToEqual(10.0).next().expectFloatToEqual(30.0);
+    cy.findByText(12).next().expectFloatToEqual(20.0).next().expectFloatToEqual(10.0).next().expectFloatToEqual(70.0);
+    cy.findByText(13).next().expectFloatToEqual(60.0).next().expectFloatToEqual(10.0).next().expectFloatToEqual(30.0);
+    cy.findByText(14).next().expectFloatToEqual(20.0).next().expectFloatToEqual(10.0).next().expectFloatToEqual(70.0);
+    cy.findByText(15).next().expectFloatToEqual(60.0).next().expectFloatToEqual(10.0).next().expectFloatToEqual(30.0);
+
+    // Submit dataset with the default dataset name
+    cy.findByText("Submit").click();
+  });
 });
