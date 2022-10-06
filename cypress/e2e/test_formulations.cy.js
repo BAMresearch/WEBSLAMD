bb8a71db85df19e1d609f08ff504698771a0e255
Horacio Scaffino <horacio.scaffino@iteratec.com>
2022-10-06 10:26:23 +0200

    Add test with admixture, custom and processes

--- cypress/e2e/test_formulations.cy.js
+++ cypress/e2e/test_formulations.cy.js
@@ -197,3 +197,90 @@ describe("Test materials formulations form", () => {
     cy.findByText("Submit").click();
   });
 });
+
+describe("Test materials formulations form with admixture, process and custom", () => {
+  beforeEach(() => {
+    cy.createExamplePowders();
+    cy.createExampleLiquids();
+    cy.createExampleAggregates();
+    cy.createExampleAdmixtures();
+    cy.createExampleCustomMaterials();
+    cy.createExampleProcesses();
+    cy.visit("http://localhost:5001/materials/formulations");
+    cy.findByText("Example Powder 1").should("exist");
+    cy.findByText("Example Powder 2").should("exist");
+    cy.findByText("Example Liquid 1").should("exist");
+    cy.findByText("Example Liquid 2").should("exist");
+    cy.findByText("Example Aggregates 1").should("exist");
+    cy.findByText("Example Aggregates 2").should("exist");
+    cy.findByText("Example Admixture 1").should("exist");
+    cy.findByText("Example Admixture 2").should("exist");
+    cy.findByText("Example Custom 1").should("exist");
+    cy.findByText("Example Custom 2").should("exist");
+    cy.findByText("Example Custom 3").should("exist");
+    cy.findByText("Example Process 1").should("exist");
+    cy.findByText("Example Process 2").should("exist");
+  });
+
+  it("Create formulations and submit them without a dataset name", () => {
+    cy.findByLabelText("1.1 - Powders (select one at least)").select("Example Powder 1");
+    cy.findByLabelText("1.2 - Liquids (select one at least)").select("Example Liquid 1");
+    cy.findByLabelText("1.3 - Aggregates (select one at least)").select("Example Aggregates 1");
+    cy.findByLabelText("1.4 - Admixture (optional)").select("Example Admixture 1");
+    cy.findByLabelText("1.5 - Custom (optional)").select(["Example Custom 1", "Example Custom 2", "Example Custom 3"]);
+    cy.findByLabelText("1.6 - Processes (optional)").select(["Example Process 1", "Example Process 2"]);
+    cy.findByLabelText("1.7 - Constraint (Sum of materials used for formulation) (kg) *")
+      .type("100")
+      .should("have.value", "100");
+
+    cy.intercept("/materials/formulations/add_min_max_entries").as("add_min_max_entries");
+    cy.findByText("4 - Configure weights for each material type").click();
+    // Wait for the modal animation to finish
+    cy.wait(400);
+    cy.findByText("Change Selection").should("exist");
+    cy.findByText("Do you really want to change the chosen selection?").should("exist");
+    cy.findByText("Close").should("exist");
+    cy.findByText("Confirm").click();
+    cy.wait("@add_min_max_entries");
+
+    // Fill in the increment, min, max values
+    cy.findAllByLabelText("Increment (kg)").first().type(5).should("have.value", 5);
+    cy.findAllByLabelText("Min (kg)").first().type(20).should("have.value", 20);
+    cy.findAllByLabelText("Max (kg)").first().type(20).should("have.value", 20);
+    cy.findAllByLabelText("Increment (kg)").eq(1).type(5).should("have.value", 5);
+    cy.findAllByLabelText("Min (kg)").eq(1).type(20).should("have.value", 20);
+    cy.findAllByLabelText("Max (kg)").eq(1).type(20).should("have.value", 20);
+    cy.findAllByLabelText("Increment (kg)").eq(2).type(5).should("have.value", 5);
+    cy.findAllByLabelText("Min (kg)").eq(2).type(20).should("have.value", 20);
+    cy.findAllByLabelText("Max (kg)").eq(2).type(20).should("have.value", 20);
+    cy.findAllByLabelText("Increment (kg)").eq(3).type(5).should("have.value", 5);
+    cy.findAllByLabelText("Min (kg)").eq(3).type(20).should("have.value", 20);
+    cy.findAllByLabelText("Max (kg)").eq(3).type(20).should("have.value", 20);
+    // Check the autocompletion feature
+    cy.findAllByLabelText("Min (kg)").last().should("have.value", "20.00");
+    cy.findAllByLabelText("Max (kg)").last().should("have.value", "20.00");
+    cy.intercept("/materials/formulations/add_weights").as("add_weights");
+    cy.findByText("5 - Show mixture in terms of base material composition").click().scrollIntoView();
+    cy.wait("@add_weights");
+
+    // Check that the configurations were generated correctly
+    cy.findByDisplayValue("20.0/20.0/20.0/20.0/20.0").should("exist");
+    cy.intercept("/materials/formulations/create_formulations_batch").as("create_formulations_batch");
+    cy.findByText("6 - Create material formulations for given configuration").click();
+    cy.wait("@create_formulations_batch");
+
+    // Check that the materials formulations were generated correctly
+    cy.findByText("Show / hide formulations").scrollIntoView();
+    cy.findByText("Idx_Sample").should("exist");
+    // Check the first 4 columns for every row
+    cy.findByText(0).next().expectFloatToEqual(20.0).next().expectFloatToEqual(20.0).next().expectFloatToEqual(20.0);
+    cy.findByText(1).next().expectFloatToEqual(20.0).next().expectFloatToEqual(20.0).next().expectFloatToEqual(20.0);
+    cy.findByText(2).next().expectFloatToEqual(20.0).next().expectFloatToEqual(20.0).next().expectFloatToEqual(20.0);
+    cy.findByText(3).next().expectFloatToEqual(20.0).next().expectFloatToEqual(20.0).next().expectFloatToEqual(20.0);
+    cy.findByText(4).next().expectFloatToEqual(20.0).next().expectFloatToEqual(20.0).next().expectFloatToEqual(20.0);
+    cy.findByText(5).next().expectFloatToEqual(20.0).next().expectFloatToEqual(20.0).next().expectFloatToEqual(20.0);
+
+    // Submit dataset with the default dataset name
+    cy.findByText("Submit").click();
+  });
+});
