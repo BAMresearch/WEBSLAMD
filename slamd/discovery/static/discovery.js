8d809c44ec6af5eb9ddfc76266a7979acc1cf166
RugTehseen <tehseen.rug@iteratec.com>
2022-10-10 14:28:21 +0200

    slamd-112: new js file for discovery utils

--- slamd/discovery/static/discovery.js
+++ slamd/discovery/static/discovery.js
@@ -10,34 +10,12 @@ async function deleteDataset(datasetName) {
     await deleteDataAndEmbedTemplateInPlaceholder(url, "datasets-table-placeholder");
 }
 
-function filterUnselectedOptionsAndAssignToSelectElement(options, selectorElementID) {
-    let options_not_selected = "";
-
-    for (const option of options) {
-        if (!option.selected) {
-            options_not_selected += `<option value="${option.value}">${option.value}</option>`;
-        }
-    }
-
-    document.getElementById(selectorElementID).innerHTML = options_not_selected;
-}
-
 function updateTargetPropertiesChoices(event) {
     filterUnselectedOptionsAndAssignToSelectElement(event.target.options, "target_properties");
 }
 
 function updateAPrioriInformationChoices(event) {
     filterUnselectedOptionsAndAssignToSelectElement(event.target.options, "a_priori_information");
-}
-
-function collectSelectedValues(options) {
-    const values = [];
-    for (const option of options) {
-        if (option.selected) {
-            values.push(option.value);
-        }
-    }
-    return values;
 }
 
 async function getTargetConfigurationForm(event, placeholderId) {
@@ -76,47 +54,10 @@ function onChangeAPrioriInformation(event) {
     getAPrioriInformationConfigurationForm(event, "a-priori-information-configuration-form-placeholder");
 }
 
-function getMaxMinValue(maxCheckboxId, minCheckboxId) {
-    const maxCheckboxElem = document.getElementById(maxCheckboxId);
-    const minCheckboxElem = document.getElementById(minCheckboxId);
-
-    if (maxCheckboxElem.checked) {
-        return maxCheckboxElem.value;
-    } else if (minCheckboxElem.checked) {
-        return minCheckboxElem.value;
-    } else {
-        throw "Invalid state";
-    }
-}
-
-function parseTargetConfigurations(numberTargetProperties) {
-    const result = [];
-    for (let i = 0; i < numberTargetProperties; ++i) {
-        const max_or_min = getMaxMinValue(
-            `target_configurations-${i}-max_or_min-0`,
-            `target_configurations-${i}-max_or_min-1`
-        );
-        const weight = document.getElementById(`target_configurations-${i}-weight`).value;
-        const threshold = document.getElementById(`target_configurations-${i}-threshold`).value;
-        result.push({max_or_min, weight, threshold});
-    }
-    return result;
-}
-
-function parseAPrioriInformationConfigurations(numberAPrioriInformationProperties) {
-    const result = [];
-    for (let i = 0; i < numberAPrioriInformationProperties; ++i) {
-        const max_or_min = getMaxMinValue(
-            `a_priori_information_configurations-${i}-max_or_min-0`,
-            `a_priori_information_configurations-${i}-max_or_min-1`
-        );
-        const weight = document.getElementById(`a_priori_information_configurations-${i}-weight`).value;
-        const threshold = document.getElementById(`a_priori_information_configurations-${i}-threshold`).value;
-        result.push({max_or_min, weight, threshold});
-    }
-    return result;
-}
-
+/**
+ * In order to be consistent with Plotly's plot function, we use the fetch api slightly different as compared to
+ * our other usecases where we use fetchDataAndEmbedTemplateInPlaceholder defined in global.js
+ */
 async function tsnePlotListener() {
     const alreadyFetched = document.getElementById("tsne-plot-placeholder").innerHTML.trim() !== ""
 
@@ -151,32 +92,6 @@ async function runExperiment() {
     plotJsonDataInPlaceholder("scatter-plot-placeholder");
 
     document.getElementById("tsne-plot-button").addEventListener('click', tsnePlotListener)
-}
-
-function createRunExperimentRequest() {
-    const materials_data_input = collectSelectedValues(document.getElementById("materials_data_input").options);
-    const target_properties = collectSelectedValues(document.getElementById("target_properties").options);
-    const a_priori_information = collectSelectedValues(document.getElementById("a_priori_information").options);
-    const model = collectSelectedValues(document.getElementById("model").options);
-    const curiosity = document.getElementById("curiosity").value;
-    const target_configurations = parseTargetConfigurations(target_properties.length);
-    const a_priori_information_configurations = parseAPrioriInformationConfigurations(a_priori_information.length);
-
-    return {
-        materials_data_input,
-        target_properties,
-        a_priori_information,
-        model: model[0],
-        curiosity,
-        target_configurations,
-        a_priori_information_configurations,
-    };
-}
-
-function plotJsonDataInPlaceholder(placeholderId) {
-    const plotJson = JSON.parse(document.getElementById(placeholderId).textContent);
-    removeInnerHtmlFromPlaceholder(placeholderId);
-    Plotly.plot(placeholderId, plotJson.data, plotJson.layout, {responsive: true});
 }
 
 function toggleRunExperimentButton() {
