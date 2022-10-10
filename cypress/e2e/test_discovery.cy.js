1d4a7f413e509d9c314ceeb03b4666c92bfed61f
Horacio Scaffino <horacio.scaffino@iteratec.com>
2022-10-10 15:02:15 +0200

    Added tests with thresholds

--- cypress/e2e/test_discovery.cy.js
+++ cypress/e2e/test_discovery.cy.js
@@ -241,6 +241,65 @@ describe("Test running experiments with example dataset", () => {
       });
   });
 
+  it("One target property and one a priori information minimized with two thresholds works", () => {
+    // Select features
+    cy.findByLabelText("Materials Data (Input) (select one column at least)").select([
+      "SiO2",
+      "CaO",
+      "SO3",
+      "FA (kg/m3)",
+      "GGBFS (kg/m3)",
+      "Coarse aggregate (kg/m3)",
+      "Fine aggregate (kg/m3)",
+      "Total aggregates",
+      "Na2SiO3",
+      "Na2O (Dry)",
+      "Sio2 (Dry)",
+      "Superplasticizer",
+      "water -eff",
+    ]);
+
+    // Select target properties
+    cy.selectInputWaitForAsyncRequest(
+      "Target Properties (select one column at least)",
+      ["fc 28-d - Target (MPa)"],
+      "materials/discovery/create_target_configuration_form"
+    );
+
+    // Select a priori information
+    cy.selectInputWaitForAsyncRequest(
+      "A priori Information (optional)",
+      ["CO2 (kg/t) - A-priori Information"],
+      "materials/discovery/create_a_priori_information_configuration_form"
+    );
+
+    // Minimize CO2 and set thresholds
+    cy.findAllByLabelText("Minimize").eq(1).check();
+    cy.fillForm({
+      Threshold: [70, 130],
+    });
+
+    // Run the experiment, wait for the request to complete
+    cy.clickButtonWaitForAsyncRequest(
+      "Run experiment with given configuration",
+      "materials/discovery/MaterialsDiscoveryExampleData.csv"
+    );
+    cy.get(".spinner-border").should("not.exist");
+
+    // Check the first three rows for the columns
+    // [Row number, Utility, Novelty, fc 28-d - Target (MPa), Uncertainty (fc 28-d - Target (MPa)), CO2 (kg/t) - A-priori Information]
+    cy.get(".table-responsive")
+      .eq(1)
+      .scrollIntoView()
+      .within(() => {
+        cy.checkGeneratedTable([
+          [1, 3.322088, 0.705232, 64.10796, 1.71396, 116.12407],
+          [2, 3.321237, 0.70523, 64.099414, 1.71567, 116.12407],
+          [3, 2.998878, 0.795733, 57.296066, 4.27784, 114.39607],
+        ]);
+      });
+  });
+
   it("Two target properties and one a priori information minimized works", () => {
     // Select features
     cy.findByLabelText("Materials Data (Input) (select one column at least)").select([
@@ -305,6 +364,63 @@ describe("Test running experiments with example dataset", () => {
           [1, 6.061092, 0.224525, 181.669628, 64.10796, 8.29994, 1.71396, 116.12407],
           [2, 6.057715, 0.224525, 181.588483, 64.099414, 8.30358, 1.71567, 116.12407],
           [3, 5.137301, 0.311907, 151.522781, 57.296066, 22.57863, 4.27784, 114.39607],
+        ]);
+      });
+  });
+
+  it("Two target properties and one a priori information minimized with threshold works", () => {
+    // Select features
+    cy.findByLabelText("Materials Data (Input) (select one column at least)").select([
+      "SiO2",
+      "CaO",
+      "SO3",
+      "FA (kg/m3)",
+      "GGBFS (kg/m3)",
+      "Coarse aggregate (kg/m3)",
+      "Fine aggregate (kg/m3)",
+      "Total aggregates",
+      "Na2SiO3",
+      "Na2O (Dry)",
+      "Sio2 (Dry)",
+      "Superplasticizer",
+      "water -eff",
+    ]);
+
+    // Select target properties
+    cy.selectInputWaitForAsyncRequest(
+      "Target Properties (select one column at least)",
+      ["fc 28-d - Target (MPa)", "Slump - Target (mm)"],
+      "materials/discovery/create_target_configuration_form"
+    );
+
+    // Select a priori information
+    cy.selectInputWaitForAsyncRequest(
+      "A priori Information (optional)",
+      ["CO2 (kg/t) - A-priori Information"],
+      "materials/discovery/create_a_priori_information_configuration_form"
+    );
+
+    // Minimize CO2 and set threshold
+    cy.findAllByLabelText("Minimize").eq(2).check();
+    cy.findAllByLabelText("Threshold").eq(2).type(120).should("have.value", 120);
+
+    // Run the experiment, wait for the request to complete
+    cy.clickButtonWaitForAsyncRequest(
+      "Run experiment with given configuration",
+      "materials/discovery/MaterialsDiscoveryExampleData.csv"
+    );
+    cy.get(".spinner-border").should("not.exist");
+
+    // Check the first three rows for the columns
+    // [Row number, Utility, Novelty, fc 28-d - Target (MPa), Slump - Target (mm),
+    //  Uncertainty (Slump - Target (mm)), Uncertainty (fc 28-d - Target (MPa)), CO2 (kg/t) - A-priori Information]
+    cy.get(".table-responsive")
+      .eq(1)
+      .within(() => {
+        cy.checkGeneratedTable([
+          [1, 5.313855, 0.807661, 181.669628, 64.10796, 8.29994, 1.71396, 116.12407],
+          [2, 5.310477, 0.807658, 181.588483, 64.099414, 8.30358, 1.71567, 116.12407],
+          [3, 4.418663, 1.0, 151.522781, 57.296066, 22.57863, 4.27784, 114.39607],
         ]);
       });
   });
