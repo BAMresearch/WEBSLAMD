5b1ef9cefb4ceceaca85cbe6b6e3a0e943f5de6f
Horacio Scaffino <horacio.scaffino@iteratec.com>
2022-10-10 14:34:44 +0200

    Use checkGeneratedTable for discovery tests

--- cypress/e2e/test_discovery.cy.js
+++ cypress/e2e/test_discovery.cy.js
@@ -6,24 +6,29 @@ describe("Test upload dataset form", () => {
   it("File upload works", () => {
     cy.get("input[type=file]").selectFile("examples/MaterialsDiscoveryExampleData.csv");
     cy.findByText("Upload dataset").click();
-    cy.findByText("MaterialsDiscoveryExampleData.csv").should("exist");
-    cy.findByText("Idx_Sample", { exact: false }).should("exist");
-    cy.findByText("SiO2", { exact: false }).should("exist");
-    cy.findByText("CaO", { exact: false }).should("exist");
-    cy.findByText("SO3", { exact: false }).should("exist");
-    cy.findByText("FA (kg/m3)", { exact: false }).should("exist");
-    cy.findByText("GGBFS (kg/m3)", { exact: false }).should("exist");
-    cy.findByText("Coarse aggregate (kg/m3)", { exact: false }).should("exist");
-    cy.findByText("Fine aggregate (kg/m3)", { exact: false }).should("exist");
-    cy.findByText("Total aggregates", { exact: false }).should("exist");
-    cy.findByText("Na2SiO3", { exact: false }).should("exist");
-    cy.findByText("Na2O (Dry)", { exact: false }).should("exist");
-    cy.findByText("Sio2 (Dry)", { exact: false }).should("exist");
-    cy.findByText("Superplasticizer", { exact: false }).should("exist");
-    cy.findByText("water -eff", { exact: false }).should("exist");
-    cy.findByText("Slump - Target (mm)", { exact: false }).should("exist");
-    cy.findByText("CO2 (kg/t) - A-priori Information", { exact: false }).should("exist");
-    cy.findByText("fc 28-d - Target (MPa)", { exact: false }).should("exist");
+    cy.checkGeneratedContent(
+      [
+        "MaterialsDiscoveryExampleData.csv",
+        "Idx_Sample",
+        "SiO2",
+        "CaO",
+        "SO3",
+        "FA (kg/m3)",
+        "GGBFS (kg/m3)",
+        "Coarse aggregate (kg/m3)",
+        "Fine aggregate (kg/m3)",
+        "Total aggregates",
+        "Na2SiO3",
+        "Na2O (Dry)",
+        "Sio2 (Dry)",
+        "Superplasticizer",
+        "water -eff",
+        "Slump - Target (mm)",
+        "CO2 (kg/t) - A-priori Information",
+        "fc 28-d - Target (MPa)",
+      ],
+      false
+    );
   });
 
   it("Delete dataset works", () => {
@@ -91,37 +96,16 @@ describe("Test running experiments with example dataset", () => {
     );
     cy.get(".spinner-border").should("not.exist");
 
-    // Check the first three rows for the columns [Row number, Utility, Novelty, fc 28-d - Target (MPa)]
-    // while also checking [CO2 (kg/t) - A-priori Information]
+    // Check the first three rows for the columns
+    // [Row number, Utility, Novelty, fc 28-d - Target (MPa), Uncertainty (fc 28-d - Target (MPa))]
     cy.get(".table-responsive")
       .eq(1)
-      .scrollIntoView()
       .within(() => {
-        cy.findByText(1)
-          .next()
-          .expectFloatToEqual(2.531052)
-          .next()
-          .expectFloatToEqual(0.599547)
-          .next()
-          .expectFloatToEqual(63.442981);
-        // This number appears twice
-        cy.findAllByText(160.79328);
-        cy.findByText(2)
-          .next()
-          .expectFloatToEqual(2.372714)
-          .next()
-          .expectFloatToEqual(0.620889)
-          .next()
-          .expectFloatToEqual(59.014729);
-        cy.findByText(164.35337);
-        cy.findByText(3)
-          .next()
-          .expectFloatToEqual(2.243499)
-          .next()
-          .expectFloatToEqual(0.523664)
-          .next()
-          .expectFloatToEqual(59.452994);
-        cy.findAllByText(160.79328);
+        cy.checkGeneratedTable([
+          [1, 2.531052, 0.599547, 63.442981, 6.00593],
+          [2, 2.372714, 0.620889, 59.014729, 9.16342],
+          [3, 2.243499, 0.523664, 59.452994, 7.68811],
+        ]);
       });
   });
 
@@ -176,37 +160,16 @@ describe("Test running experiments with example dataset", () => {
     );
     cy.get(".spinner-border").should("not.exist");
 
-    // Check the first three rows for the columns [Row number, Utility, Novelty, fc 28-d - Target (MPa)]
-    // while also checking [CO2 (kg/t) - A-priori Information]
+    // Check the first three rows for the columns
+    // [Row number, Utility, Novelty, fc 28-d - Target (MPa), Uncertainty (fc 28-d - Target (MPa)), CO2 (kg/t) - A-priori Information]
     cy.get(".table-responsive")
       .eq(1)
-      .scrollIntoView()
       .within(() => {
-        cy.findByText(1)
-          .next()
-          .expectFloatToEqual(4.048724)
-          .next()
-          .expectFloatToEqual(0.620889)
-          .next()
-          .expectFloatToEqual(59.014729);
-        cy.findByText(164.35337);
-        cy.findByText(2)
-          .next()
-          .expectFloatToEqual(4.037003)
-          .next()
-          .expectFloatToEqual(0.789527)
-          .next()
-          .expectFloatToEqual(54.640386);
-        cy.findByText(167.90098);
-        cy.findByText(3)
-          .next()
-          .expectFloatToEqual(3.954029)
-          .next()
-          .expectFloatToEqual(0.599547)
-          .next()
-          .expectFloatToEqual(63.442981);
-        // This number appears twice
-        cy.findAllByText(160.79328);
+        cy.checkGeneratedTable([
+          [1, 4.048724, 0.620889, 59.014729, 9.16342, 164.35337],
+          [2, 4.037003, 0.789527, 54.640386, 11.42005, 167.90098],
+          [3, 3.954029, 0.599547, 63.442981, 6.00593, 160.79328],
+        ]);
       });
   });
 
@@ -264,38 +227,17 @@ describe("Test running experiments with example dataset", () => {
     );
     cy.get(".spinner-border").should("not.exist");
 
-    // Check the first three rows for the columns [Row number, Utility, Novelty, fc 28-d - Target (MPa)]
-    // while also checking [CO2 (kg/t) - A-priori Information]
+    // Check the first three rows for the columns
+    // [Row number, Utility, Novelty, fc 28-d - Target (MPa), Uncertainty (fc 28-d - Target (MPa)), CO2 (kg/t) - A-priori Information]
     cy.get(".table-responsive")
       .eq(1)
       .scrollIntoView()
       .within(() => {
-        cy.findByText(1)
-          .next()
-          .expectFloatToEqual(3.831002)
-          .next()
-          .expectFloatToEqual(0.224525)
-          .next()
-          .expectFloatToEqual(64.10796);
-        // This number appears twice
-        cy.findAllByText(116.12407);
-        cy.findByText(2)
-          .next()
-          .expectFloatToEqual(3.83015)
-          .next()
-          .expectFloatToEqual(0.224525)
-          .next()
-          .expectFloatToEqual(64.099414);
-        cy.findAllByText(116.12407);
-        cy.findByText(3)
-          .next()
-          .expectFloatToEqual(3.424517)
-          .next()
-          .expectFloatToEqual(0.311907)
-          .next()
-          .expectFloatToEqual(57.296066);
-        // This number appears twice
-        cy.findAllByText(114.39607);
+        cy.checkGeneratedTable([
+          [1, 3.831002, 0.224525, 64.10796, 1.71396, 116.12407],
+          [2, 3.83015, 0.224525, 64.099414, 1.71567, 116.12407],
+          [3, 3.424517, 0.311907, 57.296066, 4.27784, 114.39607],
+        ]);
       });
   });
 
@@ -353,38 +295,17 @@ describe("Test running experiments with example dataset", () => {
     );
     cy.get(".spinner-border").should("not.exist");
 
-    // Check the first three rows for the columns [Row number, Utility, Novelty, Slump - Target (mm)]
-    // while also checking [CO2 (kg/t) - A-priori Information]
+    // Check the first three rows for the columns
+    // [Row number, Utility, Novelty, fc 28-d - Target (MPa), Slump - Target (mm),
+    //  Uncertainty (Slump - Target (mm)), Uncertainty (fc 28-d - Target (MPa)), CO2 (kg/t) - A-priori Information]
     cy.get(".table-responsive")
       .eq(1)
-      .scrollIntoView()
       .within(() => {
-        cy.findByText(1)
-          .next()
-          .expectFloatToEqual(6.061092)
-          .next()
-          .expectFloatToEqual(0.224525)
-          .next()
-          .expectFloatToEqual(181.669628);
-        // This number appears twice
-        cy.findAllByText(116.12407);
-        cy.findByText(2)
-          .next()
-          .expectFloatToEqual(6.057715)
-          .next()
-          .expectFloatToEqual(0.224525)
-          .next()
-          .expectFloatToEqual(181.588483);
-        cy.findAllByText(116.12407);
-        cy.findByText(3)
-          .next()
-          .expectFloatToEqual(5.137301)
-          .next()
-          .expectFloatToEqual(0.311907)
-          .next()
-          .expectFloatToEqual(151.522781);
-        // This number appears twice
-        cy.findAllByText(114.39607);
+        cy.checkGeneratedTable([
+          [1, 6.061092, 0.224525, 181.669628, 64.10796, 8.29994, 1.71396, 116.12407],
+          [2, 6.057715, 0.224525, 181.588483, 64.099414, 8.30358, 1.71567, 116.12407],
+          [3, 5.137301, 0.311907, 151.522781, 57.296066, 22.57863, 4.27784, 114.39607],
+        ]);
       });
   });
 });
