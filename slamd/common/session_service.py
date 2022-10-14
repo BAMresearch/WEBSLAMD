f496786f0188a9a093f88da8218d3dbda0f36274
Stefan Lueders <stefan.lueders@iteratec.com>
2022-10-14 13:19:53 +0200

    slamd-89: Refactored material session_service.py

--- slamd/common/session_service.py
+++ slamd/common/session_service.py
@@ -1,7 +1,5 @@
 import json
 
-from flask import session
-
 from slamd.common.error_handling import MaterialNotFoundException
 from slamd.discovery.processing.discovery_persistence import DiscoveryPersistence
 from slamd.discovery.processing.models.dataset import Dataset
@@ -14,20 +12,21 @@ from slamd.materials.processing.models.liquid import Liquid
 from slamd.materials.processing.models.powder import Powder
 from slamd.materials.processing.models.process import Process
 
+JSON_MAT_PROC_KEY = 'Materials_and_Processes'
+JSON_DATA_KEY = 'Datasets'
+
 
 class SessionService:
-    JSON_MAT_PROC_KEY = 'Materials_and_Processes'
-    JSON_DATA_KEY = 'Datasets'
 
     @classmethod
-    def save_session(cls):
+    def convert_session_to_json_string(cls):
         all_materials = MaterialsPersistence.find_all_materials()
         all_processes = MaterialsPersistence.find_all_processes()
         all_datasets = DiscoveryPersistence.find_all_datasets()
 
         full_json = {
-            cls.JSON_MAT_PROC_KEY: [],
-            cls.JSON_DATA_KEY: []
+            JSON_MAT_PROC_KEY: [],
+            JSON_DATA_KEY: []
         }
 
         for mat_list in all_materials:
@@ -44,15 +43,19 @@ class SessionService:
         return full_string
 
     @classmethod
-    def load_session(cls, session_data_json):
-        session_data = json.loads(session_data_json)
+    def load_session_from_json_string(cls, session_data_string):
+        session_data = json.loads(session_data_string)
 
-        loaded_materials = [] # Collect all first in case of issues
+        # Collect all data first, then write into session, in case there is a problem while loading
+        loaded_materials = []
         loaded_datasets = []
 
-        # TODO add comment - no facade
-        for dictionary in session_data[cls.JSON_MAT_PROC_KEY]:
+        # We decided to work with Material objects directly rather than building a facade for the material module:
+        # "common" part of the code has knowledge about other parts; there is less overhead this way; the interaction
+        # between this and materials is not part of the domain logic
+        for dictionary in session_data[JSON_MAT_PROC_KEY]:
             material_type = dictionary['type'].lower()
+
             if material_type == MaterialType.POWDER.value:
                 material = Powder()
             elif material_type == MaterialType.LIQUID.value:
@@ -66,12 +69,12 @@ class SessionService:
             elif material_type == MaterialType.CUSTOM.value:
                 material = Custom()
             else:
-                raise MaterialNotFoundException('The requested type is not supported!')
+                raise MaterialNotFoundException(f'The requested type {material_type} is not supported!')
 
             material.from_dict(dictionary)
             loaded_materials.append((material_type, material))
 
-        for dictionary in session_data[cls.JSON_DATA_KEY]:
+        for dictionary in session_data[JSON_DATA_KEY]:
             dataset = Dataset()
             dataset.from_dict(dictionary)
             loaded_datasets.append(dataset)
@@ -81,12 +84,3 @@ class SessionService:
 
         for dataset in loaded_datasets:
             DiscoveryPersistence.save_dataset(dataset)
-
-
-
-
-# Things to save: All base materials (Powders, Liquids, ...)
-# All blended materials
-# All datasets (formulations temp data set included)
-# TSNE plot data? predictions? CSRF token?
-# dataclass -> to dict function?
