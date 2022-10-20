23fc4d27ddfe33c9a72eb40daf0dc51737081001
RugTehseen <tehseen.rug@iteratec.com>
2022-10-20 11:40:53 +0200

    slamd-131: remove redundant method

--- slamd/formulations/processing/concrete_strategy.py
+++ slamd/formulations/processing/concrete_strategy.py
@@ -1,12 +1,8 @@
-from slamd.common.error_handling import ValueNotSupportedException, SlamdRequestTooLargeException
-from slamd.common.ml_utils import concat
-from slamd.common.slamd_utils import empty
+from slamd.common.error_handling import ValueNotSupportedException
 from slamd.discovery.processing.discovery_facade import DiscoveryFacade, TEMPORARY_CONCRETE_FORMULATION
-from slamd.formulations.processing.building_material_strategy import BuildingMaterialStrategy, WEIGHT_FORM_DELIMITER
+from slamd.formulations.processing.building_material_strategy import BuildingMaterialStrategy
 from slamd.formulations.processing.forms.concrete_selection_form import ConcreteSelectionForm
 from slamd.formulations.processing.forms.formulations_min_max_form import FormulationsMinMaxForm
-from slamd.formulations.processing.forms.weights_form import WeightsForm
-from slamd.formulations.processing.weight_input_preprocessor import MAX_NUMBER_OF_WEIGHTS
 from slamd.formulations.processing.weights_calculator import WeightsCalculator
 from slamd.materials.processing.materials_facade import MaterialsFacade
 
@@ -73,30 +69,6 @@ class ConcreteStrategy(BuildingMaterialStrategy):
         min_max_form.liquid_info_entry.data = 'Liquids ({0})'.format(', '.join(liquid_names))
 
         return min_max_form
-
-    @classmethod
-    def populate_weights_form(cls, weights_request_data):
-        materials_formulation_config = weights_request_data['materials_formulation_configuration']
-        weight_constraint = weights_request_data['weight_constraint']
-
-        # the result of the computation contains a list of lists with each containing the weights in terms of the
-        # various materials used for blending; for example weight_combinations =
-        # "[['18.2', '15.2', '66.6'], ['18.2', '20.3', '61.5'], ['28.7', '15.2', '56.1']]"
-        if empty(weight_constraint):
-            raise ValueNotSupportedException('You must set a non-empty weight constraint!')
-        else:
-            weight_combinations = cls._get_constrained_weights(materials_formulation_config, weight_constraint)
-
-        if len(weight_combinations) > MAX_NUMBER_OF_WEIGHTS:
-            raise SlamdRequestTooLargeException(
-                f'Too many weights were requested. At most {MAX_NUMBER_OF_WEIGHTS} weights can be created!')
-
-        weights_form = WeightsForm()
-        for i, entry in enumerate(weight_combinations):
-            ratio_form_entry = weights_form.all_weights_entries.append_entry()
-            ratio_form_entry.weights.data = WEIGHT_FORM_DELIMITER.join(entry)
-            ratio_form_entry.idx.data = str(i)
-        return weights_form
 
     @classmethod
     def create_formulation_batch(cls, formulations_data):
