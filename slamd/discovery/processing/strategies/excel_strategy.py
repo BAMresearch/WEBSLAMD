042096212519920d0463e950cb5b3b7c83981e9f
Horacio Scaffino <horacio.scaffino@iteratec.com>
2022-10-20 18:03:57 +0200

    Use dict comprehension in ExcelStrategy

--- slamd/discovery/processing/strategies/excel_strategy.py
+++ slamd/discovery/processing/strategies/excel_strategy.py
@@ -9,7 +9,7 @@ class ExcelStrategy:
     def create_prediction_excel(cls, dataset_of_prediction, prediction):
         original_data = dataset_of_prediction.dataframe
         prediction_df = prediction.dataframe
-        metadata_df = pd.DataFrame(dict([(k, pd.Series(v)) for k, v in prediction.metadata.items()]))
+        metadata_df = pd.DataFrame({k: pd.Series(v) for k, v in prediction.metadata.items()})
 
         output = BytesIO()
         with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
