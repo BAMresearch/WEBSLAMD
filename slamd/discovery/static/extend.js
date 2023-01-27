9b750ce0fc26909e0742eaff657a1c1a6c6d5730
Ghezal Ahmad Jan Zia <ghezalahmad.zia@outlook.com>
2023-01-27 14:35:38 +0100

    min/max in one row

--- slamd/discovery/static/extend.js
+++ slamd/discovery/static/extend.js
@@ -1,18 +1,12 @@
 document.getElementById("min_value").style.display = "none";
 document.getElementById("max_value").style.display = "none";
 window.addEventListener('load', function() {
-
   var selectColumns = document.getElementById("select_columns");
   var selected_columns = new Set();
-  // Add an event listener to the select_columns element
   selectColumns.addEventListener("change", function() {
-    // Get the selected options
     var selectedOptions = selectColumns.selectedOptions;
-    // Get the min-max container element
     var minMaxContainer = document.getElementsByClassName("min-max-container")[0];
-    // Clear the min-max container
     minMaxContainer.innerHTML = "";
-    // Check if any columns are selected
     if (selectedOptions.length == 0) {
         var minValue = document.getElementById("min_value");
         var maxValue = document.getElementById("max_value");
@@ -23,27 +17,19 @@ window.addEventListener('load', function() {
         var maxValue = document.getElementById("max_value");
         if (minValue) minValue.style.display = "block";
         if (maxValue) maxValue.style.display = "block";
-
-      // Loop through the selected options
       for (var i = 0; i < selectedOptions.length; i++) {
-        // Get the column name
         var columnName = selectedOptions[i].value;
-
-        // Create the min and max elements
         var minElement = document.createElement("div");
         var maxElement = document.createElement("div");
-
-        // Add the appropriate classes to the min and max elements
-        minElement.classList.add("col-4");
-        maxElement.classList.add("col-4");
-
-        // Set the innerHTML of the min and max elements
+        var row = document.createElement("div");
+        minElement.classList.add("col-6");
+        maxElement.classList.add("col-6");
+        row.classList.add("row");
         minElement.innerHTML = '<label class="control-label">Min ' + columnName + '</label><input class="form-control" id="min_' + columnName + '" name="min_' + columnName + '" type="text">';
         maxElement.innerHTML = '<label class="control-label">Max ' + columnName + '</label><input class="form-control" id="max_' + columnName + '" name="max_' + columnName + '" type="text">';
-
-        // Append the min and max elements to the min-max container
-        minMaxContainer.appendChild(minElement);
-        minMaxContainer.appendChild(maxElement);
+        row.appendChild(minElement);
+        row.appendChild(maxElement);
+        minMaxContainer.appendChild(row);
       }
     }
   });
