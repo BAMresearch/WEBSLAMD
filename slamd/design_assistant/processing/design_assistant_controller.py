710075f83d061fdbee7e619362fbfbc29c4e107e
RugTehseen <tehseen.rug@iteratec.com>
2024-03-08 09:05:25 +0100

    SLAMD_GENAI_2: fix formatting

--- slamd/design_assistant/processing/design_assistant_controller.py
+++ slamd/design_assistant/processing/design_assistant_controller.py
@@ -8,10 +8,12 @@ design_assistant = Blueprint('design_assistant',__name__,
                                     static_url_path='static',
                                     url_prefix='/design_assistant',)
 
+
 @design_assistant.route('/', methods=['GET'])
 def design_assistant_page():
     form = DesignAssistantService.create_design_assistant_form()
     return render_template('design_assistant.html',form=form,task_form=form.task_form,import_form=form.import_form,campaign_form=form.campaign_form,)
+
 
 @design_assistant.route('/task', methods=['POST'])
 def handle_task():
