55d8f6c362aa4b06d9e4a0c46dae7dd38f4a38fc
RugTehseen <tehseen.rug@iteratec.com>
2024-03-27 16:45:56 +0100

    SLAMD_GENAI_13: update input field label

--- slamd/design_assistant/processing/forms/token_form.py
+++ slamd/design_assistant/processing/forms/token_form.py
@@ -4,4 +4,4 @@ from wtforms.fields.simple import PasswordField
 
 class TokenForm(Form):
 
-    token = PasswordField(label='Insert you Open AI API Token here')
+    token = PasswordField(label='Insert you Open AI API Key here')
